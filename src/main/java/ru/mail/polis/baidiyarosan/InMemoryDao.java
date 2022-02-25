5f1466f4d1d6c7798bd57feaefdbeae884fea71e
AirBurn <rosan1233456@gmail.com>
2022-02-26 02:52:53 +0300

    no more memory issues

--- src/main/java/ru/mail/polis/baidiyarosan/InMemoryDao.java
+++ src/main/java/ru/mail/polis/baidiyarosan/InMemoryDao.java
@@ -4,34 +4,32 @@ import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Dao;
 
 import java.nio.ByteBuffer;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.*;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.function.Function;
 
 public class InMemoryDao implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> {
 
-    private final ConcurrentNavigableMap<ByteBuffer, ByteBuffer> collection = new ConcurrentSkipListMap<>();
-
-    private final Function<Map.Entry<ByteBuffer, ByteBuffer>, BaseEntry<ByteBuffer>> mapper =
-            (e) -> new BaseEntry(e.getKey(), e.getValue());
+    private final NavigableMap<ByteBuffer, BaseEntry<ByteBuffer>> collection = new ConcurrentSkipListMap<>();
 
     @Override
     public Iterator<BaseEntry<ByteBuffer>> get(ByteBuffer from, ByteBuffer to) {
+
         if (from == null && to == null) {
-            return collection.entrySet().stream().map(mapper).iterator();
+            return collection.values().iterator();
         }
 
         return collection.subMap(
                 from == null ? collection.firstKey() : from, true,
                 to == null ? collection.lastKey() : to, to == null
-        ).entrySet().stream().map(mapper).iterator();
-    }
+        ).values().iterator();
+
+  }
 
     @Override
     public void upsert(BaseEntry<ByteBuffer> entry) {
-        collection.put(entry.key(), entry.value());
+        collection.put(entry.key(), entry);
     }
+
+
 
 }
