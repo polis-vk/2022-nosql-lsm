85d218acbea99cf2532f3d64707fcfd6cc375705
ArtyomScheredin <mrboxman75@gmail.com>
2022-02-25 22:41:03 +0300

    Fix codestyle

--- src/main/java/ru/mail/polis/artyomscheredin/inMemoryDAO.java
+++ src/main/java/ru/mail/polis/artyomscheredin/inMemoryDAO.java
@@ -1,18 +1,10 @@
 package ru.mail.polis.artyomscheredin;
 
 import java.nio.ByteBuffer;
-import java.util.AbstractMap;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.stream.Collectors;
 
 import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Dao;
@@ -33,10 +25,7 @@ public class inMemoryDAO implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> {
         } else if (to == null) {
             return data.tailMap(from).values().iterator();
         }
-        var t = data.subMap(from, to);
-        int x = 0;
-        x++;
-        return t.values().iterator();
+        return data.subMap(from, to).values().iterator();
     }
 
     @Override
