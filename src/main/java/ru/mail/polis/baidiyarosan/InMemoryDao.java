4b6c07f17e9f7acf1cc10782b50feecbd2ac7728
AirBurn <rosan1233456@gmail.com>
2022-02-25 22:48:46 +0300

    first try, idk isn't it fast enough or smthng just wrong with the tests

---
+++ src/main/java/ru/mail/polis/baidiyarosan/InMemoryDao.java
@@ -0,0 +1,36 @@
+package ru.mail.polis.baidiyarosan;
+
+import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Dao;
+
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.function.Function;
+
+public class InMemoryDao implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> {
+
+    private final ConcurrentNavigableMap<ByteBuffer, ByteBuffer> collection = new ConcurrentSkipListMap<>();
+
+    private final Function<Map.Entry<ByteBuffer, ByteBuffer>, BaseEntry<ByteBuffer>> mapper = (e) -> new BaseEntry(e.getKey(), e.getValue());
+
+    @Override
+    public Iterator<BaseEntry<ByteBuffer>> get(ByteBuffer from, ByteBuffer to) {
+        if (from == null && to == null) {
+            return collection.entrySet().stream().map(mapper).iterator();
+        }
+
+        return collection.subMap(
+                from == null ? collection.firstKey() : from, true,
+                to == null ? collection.lastKey() : to, to == null
+        ).entrySet().stream().map(mapper).iterator();
+    }
+
+    @Override
+    public void upsert(BaseEntry<ByteBuffer> entry) {
+        collection.put(entry.key(), entry.value());
+    }
+
+}
