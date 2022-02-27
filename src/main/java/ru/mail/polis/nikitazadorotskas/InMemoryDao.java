d7f3b783a7a578078acb6d465d09722f569c665e
zadorotskas <zadorotskas@yandex.ru>
2022-02-27 14:41:35 +0300

    fixes in InMemoryDao

--- src/main/java/ru/mail/polis/nikitazadorotskas/InMemoryDao.java
+++ src/main/java/ru/mail/polis/nikitazadorotskas/InMemoryDao.java
@@ -1,69 +1,77 @@
 package ru.mail.polis.nikitazadorotskas;
 
+import jdk.incubator.foreign.MemoryAccess;
 import jdk.incubator.foreign.MemorySegment;
-import jdk.internal.foreign.AbstractMemorySegmentImpl;
 import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Dao;
 
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<MemorySegment, BaseEntry<MemorySegment>> {
-    private final ConcurrentNavigableMap<AbstractMemorySegmentImpl, AbstractMemorySegmentImpl> memory
+    private final ConcurrentNavigableMap<MemorySegment, MemorySegment> memory
             = new ConcurrentSkipListMap<>(this::compareMemorySegment);
 
-    private int compareMemorySegment(AbstractMemorySegmentImpl first, AbstractMemorySegmentImpl second) {
-        return Arrays.compare(toByteArray(first), toByteArray(second));
-    }
+    private int compareMemorySegment(MemorySegment first, MemorySegment second) {
+        long firstMismatchByte = first.mismatch(second);
 
-    private byte[] toByteArray(AbstractMemorySegmentImpl segment) {
-        return (byte[]) segment.unsafeGetBase();
+        if (firstMismatchByte == -1) {
+            return 0;
+        }
+
+        byte firstByte = MemoryAccess.getByteAtOffset(first, firstMismatchByte);
+        byte secondByte = MemoryAccess.getByteAtOffset(second, firstMismatchByte);
+
+        return Byte.compare(firstByte, secondByte);
     }
 
     @Override
     public Iterator<BaseEntry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        return getIteratorFromMap(getMap(from, to));
+        return new BaseEntryIterator(getMap(from, to));
     }
 
-    private ConcurrentNavigableMap<AbstractMemorySegmentImpl, AbstractMemorySegmentImpl> getMap(
-            MemorySegment from, MemorySegment to
-    ) {
+    private ConcurrentNavigableMap<MemorySegment, MemorySegment> getMap(MemorySegment from, MemorySegment to) {
         if (from == null && to == null) {
             return memory;
         }
 
         if (from == null) {
-            return memory.headMap(cast(to));
+            return memory.headMap(to);
         }
 
         if (to == null) {
-            return memory.tailMap(cast(from));
+            return memory.tailMap(from);
         }
 
-        return memory.subMap(cast(from), cast(to));
+        return memory.subMap(from, to);
     }
 
-    private Iterator<BaseEntry<MemorySegment>> getIteratorFromMap(
-            ConcurrentNavigableMap<AbstractMemorySegmentImpl, AbstractMemorySegmentImpl> map
-    ) {
-        return map.entrySet().stream()
-                .map(this::toBaseEntry)
-                .iterator();
+    private class BaseEntryIterator implements Iterator<BaseEntry<MemorySegment>> {
+        private final Iterator<Entry<MemorySegment, MemorySegment>> iterator;
+
+        private BaseEntryIterator(ConcurrentNavigableMap<MemorySegment, MemorySegment> map) {
+            iterator = map.entrySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public BaseEntry<MemorySegment> next() {
+            return toBaseEntry(iterator.next());
+        }
     }
 
-    private BaseEntry<MemorySegment> toBaseEntry(Entry<AbstractMemorySegmentImpl, AbstractMemorySegmentImpl> entry) {
+    private BaseEntry<MemorySegment> toBaseEntry(Entry<MemorySegment, MemorySegment> entry) {
         return new BaseEntry<>(entry.getKey(), entry.getValue());
-    }
-
-    private AbstractMemorySegmentImpl cast(MemorySegment ms) {
-        return (AbstractMemorySegmentImpl) ms;
     }
 
     @Override
     public void upsert(BaseEntry<MemorySegment> entry) {
-        memory.put(cast(entry.key()), cast(entry.value()));
+        memory.put(entry.key(), entry.value());
     }
 }
