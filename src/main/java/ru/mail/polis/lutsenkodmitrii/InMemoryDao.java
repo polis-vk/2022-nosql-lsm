9ada148981e3e0d02ab26bda004f56e36d611793
LutsenkoDm <lutsenkodm@mail.ru>
2022-02-28 18:52:45 +0300

    Fix redundant allocation

--- src/main/java/ru/mail/polis/lutsenkodmitrii/InMemoryDao.java
+++ src/main/java/ru/mail/polis/lutsenkodmitrii/InMemoryDao.java
@@ -4,50 +4,29 @@ import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Dao;
 
 import java.util.Iterator;
-import java.util.Map;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<String, BaseEntry<String>> {
 
-    private final NavigableMap<String, String> data = new ConcurrentSkipListMap<>();
+    private final NavigableMap<String, BaseEntry<String>> data = new ConcurrentSkipListMap<>();
 
     @Override
     public Iterator<BaseEntry<String>> get(String from, String to) {
         if (from == null && to == null) {
-            return new DaoIterator(data.entrySet().iterator());
+            return data.values().iterator();
         }
         if (from == null) {
-            return new DaoIterator(data.headMap(to).entrySet().iterator());
+            return data.headMap(to).values().iterator();
         }
         if (to == null) {
-            return new DaoIterator(data.tailMap(from).entrySet().iterator());
+            return data.tailMap(from).values().iterator();
         }
-        return new DaoIterator(data.subMap(from, to).entrySet().iterator());
+        return data.subMap(from, to).values().iterator();
     }
 
     @Override
     public void upsert(BaseEntry<String> entry) {
-        data.put(entry.key(), entry.value());
-    }
-
-    static class DaoIterator implements Iterator<BaseEntry<String>> {
-        private final Iterator<Map.Entry<String, String>> iterator;
-        private Map.Entry<String, String> tempEntry;
-
-        DaoIterator(Iterator<Map.Entry<String, String>> iterator) {
-            this.iterator = iterator;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return iterator.hasNext();
-        }
-
-        @Override
-        public BaseEntry<String> next() {
-            tempEntry = iterator.next();
-            return new BaseEntry<>(tempEntry.getKey(), tempEntry.getValue());
-        }
+        data.put(entry.key(), entry);
     }
 }
