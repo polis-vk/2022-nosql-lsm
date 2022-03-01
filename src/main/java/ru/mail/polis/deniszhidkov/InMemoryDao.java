c98cf33030a27861264999416190d12f14214d9d
Денис Жидков <denis.zhidkov01@gmail.com>
2022-03-01 18:05:03 +0300

    First stage done

---
+++ src/main/java/ru/mail/polis/deniszhidkov/InMemoryDao.java
@@ -0,0 +1,34 @@
+package ru.mail.polis.deniszhidkov;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Dao;
+
+public class InMemoryDao implements Dao<String, BaseEntry<String>> {
+
+    ConcurrentNavigableMap<String, BaseEntry<String>> storage = new ConcurrentSkipListMap<>();
+
+    @Override
+    public Iterator<BaseEntry<String>> get(String from, String to) {
+        Collection<BaseEntry<String>> values;
+        if (from == null && to == null) {
+            values = storage.values();
+        } else if (from == null) {
+            values = storage.headMap(to).values();
+        } else if (to == null) {
+            values = storage.tailMap(from).values();
+        } else {
+            values = storage.subMap(from, to).values();
+        }
+        return values.iterator();
+    }
+
+    @Override
+    public void upsert(BaseEntry<String> entry) {
+        storage.put(entry.key(), entry);
+    }
+}
