eab6cb0a86a8565617b0e23d627598f0ff751f67
n.dergunov <n.dergunov@vk.team>
2022-03-02 04:13:28 +0300

    STAGE-1 Add implementation DAO in memory.

---
+++ src/main/java/ru/mail/polis/nikitadergunov/InMemoryDao.java
@@ -0,0 +1,44 @@
+package ru.mail.polis.nikitadergunov;
+
+import jdk.incubator.foreign.MemoryAccess;
+import jdk.incubator.foreign.MemorySegment;
+import ru.mail.polis.Dao;
+import ru.mail.polis.Entry;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
+    private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> storage =
+            new ConcurrentSkipListMap<>((MemorySegment firstSegment, MemorySegment secondSegment) -> {
+                long offsetMismatch = firstSegment.mismatch(secondSegment);
+                if (offsetMismatch == -1) {
+                    return 0;
+                }
+
+                return Character.compare(MemoryAccess.getCharAtOffset(firstSegment, offsetMismatch),
+                        MemoryAccess.getCharAtOffset(secondSegment, offsetMismatch));
+            }
+    );
+
+    @Override
+    public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
+        if (from == null && to == null) {
+            return storage.values().iterator();
+        }
+        if (from == null) {
+            return storage.headMap(to).values().iterator();
+        }
+        if (to == null) {
+            return storage.tailMap(from).values().iterator();
+        }
+        return storage.subMap(from, to).values().iterator();
+    }
+
+    @Override
+    public void upsert(Entry<MemorySegment> entry) {
+        storage.put(entry.key(), entry);
+    }
+
+}
