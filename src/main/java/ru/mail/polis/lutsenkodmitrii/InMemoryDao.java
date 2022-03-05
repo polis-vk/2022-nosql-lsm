fefad4f0079a756dff633537db6dc4af23a11796
LutsenkoDm <lutsenkodm@mail.ru>
2022-03-05 15:08:57 +0300

    Add stage 2

--- src/main/java/ru/mail/polis/lutsenkodmitrii/InMemoryDao.java
+++ src/main/java/ru/mail/polis/lutsenkodmitrii/InMemoryDao.java
@@ -1,15 +1,40 @@
 package ru.mail.polis.lutsenkodmitrii;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Stream;
 
 public class InMemoryDao implements Dao<String, BaseEntry<String>> {
 
+    private Path path;
+    private final String KEY_VALUE_SEPARATOR = ":";
     private final NavigableMap<String, BaseEntry<String>> data = new ConcurrentSkipListMap<>();
+
+    public InMemoryDao() {
+    }
+
+    public InMemoryDao(Config config) {
+        path = Path.of(config.basePath() + File.separator + "daoData.txt");
+        try (Stream<String> lines = Files.lines(path, Charset.defaultCharset())) {
+            lines.forEach(line -> {
+                String[] keyValue = line.split(KEY_VALUE_SEPARATOR);
+                data.put(keyValue[0], new BaseEntry<>(keyValue[0], keyValue[1]));
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Iterator<BaseEntry<String>> get(String from, String to) {
@@ -28,5 +53,21 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
     @Override
     public void upsert(BaseEntry<String> entry) {
         data.put(entry.key(), entry);
+    }
+
+    @Override
+    public void flush() {
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+            String stingToWrite;
+            for (BaseEntry<String> baseEntry : data.values()) {
+                stingToWrite = baseEntry.key() + KEY_VALUE_SEPARATOR + baseEntry.value() + '\n';
+                Files.writeString(path, stingToWrite, Charset.defaultCharset(), StandardOpenOption.APPEND);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
