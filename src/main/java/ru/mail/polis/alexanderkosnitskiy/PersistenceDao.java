06e79aca2895484ec144d03a1550fc302e9e4e49
Sanerins <askosnitskiy@gmail.com>
2022-03-06 02:50:33 +0300

    Make dao store data

--- src/main/java/ru/mail/polis/alexanderkosnitskiy/PersistenceDao.java
+++ src/main/java/ru/mail/polis/alexanderkosnitskiy/PersistenceDao.java
@@ -4,6 +4,7 @@ import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -20,12 +21,20 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
 
 public class PersistenceDao implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> {
-    private final ConcurrentNavigableMap<ByteBuffer, ByteBuffer> storage;
+    private ConcurrentNavigableMap<ByteBuffer, ByteBuffer> memory;
     private final Config config;
+    private int currentFile = 0;
+    private boolean readerMode = false;
 
     public PersistenceDao(Config config) {
         ConcurrentNavigableMap<ByteBuffer, ByteBuffer> temp = null;
-        try (FileInputStream in = new FileInputStream(config.basePath().toString() + "/data.txt");
+        try {
+            currentFile = new File(config.basePath().toString()).list().length;
+        } catch (NullPointerException e) {
+            currentFile = 0;
+        }
+        try (FileInputStream in = new FileInputStream(config.basePath().toString()
+                + "/data" + currentFile + ".txt");
             ObjectInputStream reader = new ObjectInputStream(in)) {
             Map<ByteBuffer, ByteBuffer> map = ((Map<byte[], byte[]>) reader.readObject()).entrySet().stream()
                     .collect(Collectors.toMap(k -> ByteBuffer.wrap(k.getKey()), v -> ByteBuffer.wrap(v.getValue())));
@@ -36,36 +45,99 @@ public class PersistenceDao implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> {
             e.printStackTrace();
         }
         this.config = config;
-        storage = temp;
+        memory = temp;
+    }
+
+    @Override
+    public BaseEntry<ByteBuffer> get(ByteBuffer key) {
+        Iterator<BaseEntry<ByteBuffer>> iterator = get(key, null);
+        if (!iterator.hasNext()) {
+            return readFromStorage(key);
+        }
+        BaseEntry<ByteBuffer> next = iterator.next();
+        if (next.key().equals(key)) {
+            return next;
+        }
+        return readFromStorage(key);
+    }
+
+    private BaseEntry<ByteBuffer> readFromStorage(ByteBuffer key) {
+        ByteBuffer res = null;
+        try {
+            res = findInFiles(key);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(res != null){
+            return new BaseEntry<>(key, res);
+        }
+        return null;
     }
 
     @Override
     public Iterator<BaseEntry<ByteBuffer>> get(ByteBuffer from, ByteBuffer to) {
         if (from == null && to == null) {
-            return new DaoIterator(storage.entrySet().iterator());
+            return new DaoIterator(memory.entrySet().iterator());
         }
         if (from == null) {
-            return new DaoIterator(storage.headMap(to, false).entrySet().iterator());
+            return new DaoIterator(memory.headMap(to, false).entrySet().iterator());
         }
         if (to == null) {
-            return new DaoIterator(storage.tailMap(from, true).entrySet().iterator());
+            return new DaoIterator(memory.tailMap(from, true).entrySet().iterator());
         }
-        return new DaoIterator(storage.subMap(from, true, to, false).entrySet().iterator());
+        return new DaoIterator(memory.subMap(from, true, to, false).entrySet().iterator());
     }
 
     @Override
     public void upsert(BaseEntry<ByteBuffer> entry) {
-        storage.put(entry.key(), entry.value());
+        if(readerMode) {
+            readerMode = false;
+            memory.clear();
+        }
+        if(memory.size() >= 20000) {
+            try {
+                flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        memory.put(entry.key(), entry.value());
     }
 
     @Override
     public void flush() throws IOException {
-        try (FileOutputStream out = new FileOutputStream(config.basePath().toString() + "/data.txt");
+        try (FileOutputStream out = new FileOutputStream(config.basePath().toString()
+                + "/data" + currentFile + ".txt");
             ObjectOutputStream writer = new ObjectOutputStream(out)) {
-            HashMap<byte[], byte[]> map = (HashMap<byte[], byte[]>) storage.entrySet().stream()
+            HashMap<byte[], byte[]> map = (HashMap<byte[], byte[]>) memory.entrySet().stream()
                     .collect(Collectors.toMap(k -> k.getKey().array(), v -> v.getValue().array()));
             writer.writeObject(map);
+            memory.clear();
+            currentFile++;
         }
+    }
+
+    private ByteBuffer findInFiles(ByteBuffer key) throws IOException {
+        if(!readerMode) {
+            readerMode = true;
+            flush();
+        }
+        for(int i = currentFile - 1; i >= 0; i--) {
+            try (FileInputStream in = new FileInputStream(config.basePath().toString()
+                    + "/data" + i + ".txt");
+                 ObjectInputStream reader = new ObjectInputStream(in)) {
+                Map<ByteBuffer, ByteBuffer> map = ((Map<byte[], byte[]>) reader.readObject()).entrySet().stream()
+                        .collect(Collectors.toMap(k -> ByteBuffer.wrap(k.getKey()), v -> ByteBuffer.wrap(v.getValue())));
+                ByteBuffer value = map.get(key);
+                if(value != null) {
+                    memory = new ConcurrentSkipListMap<>(map);
+                    return value;
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
     static class DaoIterator implements Iterator<BaseEntry<ByteBuffer>> {
