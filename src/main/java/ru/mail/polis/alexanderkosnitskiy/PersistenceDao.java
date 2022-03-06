6d7bbb81923f9e35d169ef585f0e756ad57d1303
Sanerins <askosnitskiy@gmail.com>
2022-03-06 03:22:51 +0300

    Make buffer smaller

--- src/main/java/ru/mail/polis/alexanderkosnitskiy/PersistenceDao.java
+++ src/main/java/ru/mail/polis/alexanderkosnitskiy/PersistenceDao.java
@@ -97,7 +97,7 @@ public class PersistenceDao implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> {
             readerMode = false;
             memory.clear();
         }
-        if (memory.size() >= 20000) {
+        if (memory.size() >= 10000) {
             try {
                 flush();
             } catch (IOException e) {
