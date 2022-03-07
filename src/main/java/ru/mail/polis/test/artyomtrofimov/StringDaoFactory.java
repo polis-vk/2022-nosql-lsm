0eaa73776f7030e3022676698ed292851d53a7ef
Artyom Trofimov <artem24630@gmail.com>
2022-03-08 01:27:32 +0300

    small fix

--- src/main/java/ru/mail/polis/test/artyomtrofimov/StringDaoFactory.java
+++ src/main/java/ru/mail/polis/test/artyomtrofimov/StringDaoFactory.java
@@ -1,13 +1,12 @@
 package ru.mail.polis.test.artyomtrofimov;
 
-import java.io.IOException;
-
 import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 import ru.mail.polis.artyomtrofimov.InMemoryDao;
 import ru.mail.polis.test.DaoFactory;
+import java.io.IOException;
 
 @DaoFactory(stage = 2)
 public class StringDaoFactory implements DaoFactory.Factory<String, Entry<String>> {
