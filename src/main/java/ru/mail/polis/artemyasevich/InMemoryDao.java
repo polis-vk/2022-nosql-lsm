3c4f527a189d8094225326a6377cc13f4f5cebf6
Artem <artem.yasevich@mail.ru>
2022-03-07 12:48:57 +0300

    Stage 2 completed

--- src/main/java/ru/mail/polis/artemyasevich/InMemoryDao.java
+++ src/main/java/ru/mail/polis/artemyasevich/InMemoryDao.java
@@ -1,15 +1,34 @@
 package ru.mail.polis.artemyasevich;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<String, BaseEntry<String>> {
-    ConcurrentNavigableMap<String, BaseEntry<String>> dataMap = new ConcurrentSkipListMap<>();
+    private static final int MAX_SIZE = 100_000;
+    private static final String FILE_NAME = "data";
+
+    private final ConcurrentNavigableMap<String, BaseEntry<String>> dataMap = new ConcurrentSkipListMap<>();
+    private final Path pathToDirectory;
+    private int fileToWrite;
+
+    public InMemoryDao(Config config) throws IOException {
+        this.pathToDirectory = config.basePath();
+        File[] files = pathToDirectory.toFile().listFiles();
+        fileToWrite = files == null ? 0 : files.length;
+    }
+
+    public InMemoryDao() {
+        this.pathToDirectory = null;
+    }
 
     @Override
     public Iterator<BaseEntry<String>> get(String from, String to) {
@@ -27,7 +46,79 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
     }
 
     @Override
+    public BaseEntry<String> get(String key) throws IOException {
+        BaseEntry<String> entry = dataMap.get(key);
+        if (entry != null) {
+            return entry;
+        }
+        entry = getFromFile(key);
+        return entry;
+    }
+
+    @Override
     public void upsert(BaseEntry<String> entry) {
+        if (dataMap.size() == MAX_SIZE && !dataMap.containsKey(entry.key())) {
+            try {
+                flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         dataMap.put(entry.key(), entry);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        savaData();
+        dataMap.clear();
+    }
+
+    @Override
+    public void close() throws IOException {
+        savaData();
+    }
+
+    private void savaData() throws IOException {
+        Path path = pathToDirectory.resolve(FILE_NAME + fileToWrite + ".txt");
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (BaseEntry<String> entry : dataMap.values()) {
+                writer.write(entry.key() + " " + entry.value() + "\n");
+            }
+            fileToWrite++;
+        }
+    }
+
+    private BaseEntry<String> getFromFile(String key) throws IOException {
+        BaseEntry<String> res = null;
+        for (int i = fileToWrite - 1; i >= 0; i--) {
+            Path path = pathToDirectory.resolve(FILE_NAME + i + ".txt");
+            try (RandomAccessFile reader = new RandomAccessFile(path.toFile(), "r")) {
+                long entrySize = reader.readLine().getBytes().length + 1;
+                long left = 0;
+                long middle;
+                long right = reader.length();
+                while (left < right) {
+                    middle = left + ((right - left) / entrySize) / 2 * entrySize;
+                    reader.seek(middle);
+                    String[] entry = reader.readLine().split(" ");
+                    int comparison = key.compareTo(entry[0]);
+                    if (comparison == 0) {
+                        res = new BaseEntry<>(entry[0], entry[1].trim());
+                        break;
+                    } else if (comparison > 0) {
+                        left = middle;
+                    } else {
+                        right = middle;
+                    }
+                }
+            }
+            if (res != null) {
+                break;
+            }
+        }
+        return res;
     }
 }
--- src/main/java/ru/mail/polis/test/artemyasevich/StringDaoFactory.java
+++ src/main/java/ru/mail/polis/test/artemyasevich/StringDaoFactory.java
@@ -1,12 +1,15 @@
 package ru.mail.polis.test.artemyasevich;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 import ru.mail.polis.artemyasevich.InMemoryDao;
 import ru.mail.polis.test.DaoFactory;
 
-@DaoFactory
+import java.io.IOException;
+
+@DaoFactory(stage =  2, week = 1)
 public class StringDaoFactory implements DaoFactory.Factory<String, BaseEntry<String>> {
     @Override
     public Dao<String, BaseEntry<String>> createDao() {
@@ -27,4 +30,10 @@ public class StringDaoFactory implements DaoFactory.Factory<String, BaseEntry<St
     public BaseEntry<String> fromBaseEntry(Entry<String> baseEntry) {
         return new BaseEntry<>(baseEntry.key(), baseEntry.value());
     }
+
+    @Override
+    public Dao<String, BaseEntry<String>> createDao(Config config) throws IOException {
+        return new InMemoryDao(config);
+    }
+
 }
