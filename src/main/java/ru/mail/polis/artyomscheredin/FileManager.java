02ad7a999d1e3f962beb3d319d70f441b76e7da2
ArtyomScheredin <mrboxman75@gmail.com>
2022-03-09 02:24:37 +0300

    Fix bugs

--- src/main/java/ru/mail/polis/artyomscheredin/FileManager.java
+++ src/main/java/ru/mail/polis/artyomscheredin/FileManager.java
@@ -7,11 +7,8 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.SortedMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Config;
@@ -21,7 +18,6 @@ public class FileManager {
     private static final String EXTENSION = ".txt";
 
     private Path pathToWrite;
-    private int counter;
     Config config;
 
     public FileManager(Config config) throws IOException {
@@ -37,34 +33,10 @@ public class FileManager {
                 throw new IOException();
             }
         }
-        counter = 0;
-    }
-
-    private void createDataFileIfNotExists() throws IOException {
-        pathToWrite = config.basePath().resolve(DATA_UNIT_NAME + counter + EXTENSION);
-        while (Files.exists(pathToWrite)) {
-            counter++;
-            pathToWrite = config.basePath().resolve(DATA_UNIT_NAME + counter + EXTENSION);
-        }
-        Files.createFile(pathToWrite);
     }
 
     public BaseEntry<ByteBuffer> getByKey(ByteBuffer key) throws IOException {
         return searchFile(pathToWrite.toFile(), key);
-        /*Pattern filePattern = Pattern.compile(DATA_UNIT_NAME + "[0-9]+" + EXTENSION);
-        BaseEntry<ByteBuffer> result;
-
-        File[] files = config.basePath().toFile().listFiles();
-        for (int i = files.length - 1; i >= 0; i--) {
-            Matcher matcher = filePattern.matcher(files[i].getName());
-            if (matcher.matches()) {
-                result = searchFile(files[i], key);
-                if (result != null) {
-                    return result;
-                }
-            }
-        }
-        return null;*/
     }
 
     private BaseEntry<ByteBuffer> searchFile(File file, ByteBuffer key) throws IOException {
@@ -97,14 +69,12 @@ public class FileManager {
         if (data == null) {
             throw new IllegalArgumentException();
         }
-        //createDataFileIfNotExists();
         try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(pathToWrite.toFile(), true))) {
             for (BaseEntry<ByteBuffer> e : data.values()) {
                 ByteBuffer entry = getBufferFromEntry(e);
                 out.write(entry.array());
             }
         }
-        counter++;
     }
 
     private ByteBuffer getBufferFromEntry(BaseEntry<ByteBuffer> e) {
