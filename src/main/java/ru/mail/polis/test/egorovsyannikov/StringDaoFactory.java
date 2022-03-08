150f7c0acb85db5a392be83a4ca0f3b59b0e4ace
eg.ovsyannikov <e.a.ovsya@gmail.com>
2022-03-08 18:15:17 +0300

    week 2

--- src/main/java/ru/mail/polis/test/egorovsyannikov/StringDaoFactory.java
+++ src/main/java/ru/mail/polis/test/egorovsyannikov/StringDaoFactory.java
@@ -6,7 +6,7 @@ import ru.mail.polis.Entry;
 import ru.mail.polis.egorovsyannikov.InMemoryDao;
 import ru.mail.polis.test.DaoFactory;
 
-@DaoFactory
+@DaoFactory(week = 2)
 public class StringDaoFactory implements DaoFactory.Factory<String, BaseEntry<String>> {
     @Override
     public Dao<String, BaseEntry<String>> createDao() {
