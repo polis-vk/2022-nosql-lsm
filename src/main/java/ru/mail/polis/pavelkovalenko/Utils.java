429e241e780640224962f56fe0fc0a1b92555b22
PaaavelZ <paaavel.2024@gmail.com>
2022-03-08 21:31:18 +0300

    Try to fix codeclimate. Part 4

--- src/main/java/ru/mail/polis/pavelkovalenko/Utils.java
+++ src/main/java/ru/mail/polis/pavelkovalenko/Utils.java
@@ -1,6 +1,6 @@
 package ru.mail.polis.pavelkovalenko;
 
-public class Utils {
+public final class Utils {
 
     public static int minSizeOfElem = Integer.MAX_VALUE;
     public static final char PAIR_SEPARATOR = '\n';
