03b281866d0636a8baeb73667b6c27f4c3b91cca
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2022-03-09 13:54:15 +0300

    Fix imports

--- src/test/java/ru/mail/polis/PersistentTest.java
+++ src/test/java/ru/mail/polis/PersistentTest.java
@@ -4,12 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import ru.mail.polis.test.DaoFactory;
 
 import java.io.IOException;
-import java.io.ObjectInputFilter;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
