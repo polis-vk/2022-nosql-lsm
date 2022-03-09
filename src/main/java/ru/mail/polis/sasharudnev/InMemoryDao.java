02675cb1d9e90882f6dbc61dc4fec78353cabc86
Ralex2105 <ralex_06@mail.ru>
2022-03-09 20:06:52 +0300

    stage 2 - first try

--- src/main/java/ru/mail/polis/sasharudnev/InMemoryDao.java
+++ src/main/java/ru/mail/polis/sasharudnev/InMemoryDao.java
@@ -1,8 +1,14 @@
 package ru.mail.polis.sasharudnev;
 
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentNavigableMap;
@@ -11,6 +17,12 @@ import java.util.concurrent.ConcurrentSkipListMap;
 public class InMemoryDao implements Dao<String, BaseEntry<String>> {
 
     private final ConcurrentNavigableMap<String, BaseEntry<String>> data = new ConcurrentSkipListMap<>();
+    private String path = "data.txt";
+    private static final String SEPARATOR = " ";
+
+    public InMemoryDao(Config config) throws IOException {
+        path = config.basePath().resolve(path).toString();
+    }
 
     @Override
     public Iterator<BaseEntry<String>> get(String from, String to) {
@@ -32,7 +44,47 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
     }
 
     @Override
+    public BaseEntry<String> get(String key) throws IOException {
+        if (data.containsKey(key)) {
+            return data.get(key);
+        }
+        if (Files.exists(Path.of(path))) {
+            try (BufferedReader reader = Files.newBufferedReader(Path.of(path))) {
+                String line = reader.readLine();
+                while (line != null) {
+                    String[] entry = line.split(SEPARATOR, -1);
+                    if (entry[0].equals(key)) {
+                        return new BaseEntry<>(entry[0], entry[1]);
+                    }
+                    line = reader.readLine();
+                }
+            } catch (IOException de) {
+                throw new IOException("Get" + de.getMessage());
+            }
+        }
+        return null;
+    }
+
+    @Override
     public void upsert(BaseEntry<String> entry) {
         data.put(entry.key(), entry);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(path), StandardCharsets.UTF_8)) {
+            if (Files.notExists(Path.of(path))) {
+                Files.createFile(Path.of(path));
+            }
+            for (Map.Entry<String, BaseEntry<String>> entry : data.entrySet()) {
+                String builder = entry.getKey() +
+                        SEPARATOR +
+                        entry.getValue().value() +
+                        "\n";
+                writer.write(builder);
+            }
+        } catch (Exception de) {
+            throw new IOException("Flush" + de.getMessage());
+        }
     }
 }
