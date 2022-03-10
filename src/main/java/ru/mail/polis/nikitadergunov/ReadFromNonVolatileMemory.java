3967eefa28de80e1529405814efbe5f07da65c51
n.dergunov <n.dergunov@vk.team>
2022-03-10 20:30:33 +0300

    STAGE-2 Remove syscall

--- src/main/java/ru/mail/polis/nikitadergunov/ReadFromNonVolatileMemory.java
+++ src/main/java/ru/mail/polis/nikitadergunov/ReadFromNonVolatileMemory.java
@@ -6,27 +6,25 @@ import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Config;
 import ru.mail.polis.Entry;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ReadFromNonVolatileMemory {
 
-    private final MemorySegment readMemorySegment;
-    private final boolean isExist;
+    private MemorySegment readMemorySegment;
+    private boolean isExist;
 
     public ReadFromNonVolatileMemory(Config config) throws IOException {
         Path pathToTable = config.basePath().resolve("table");
-        if (!Files.exists(pathToTable)) {
-            readMemorySegment = null;
-            isExist = false;
-            return;
-        }
         try (FileChannel readChannel = FileChannel.open(pathToTable)) {
             readMemorySegment = MemorySegment.mapFile(pathToTable, 0,
                     readChannel.size(), FileChannel.MapMode.READ_ONLY, ResourceScope.globalScope());
             isExist = true;
+        } catch (FileNotFoundException e) {
+            readMemorySegment = null;
+            isExist = false;
         }
     }
 
