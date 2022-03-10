72302cecd6b45606aa225dcc1c9916315038ee3c
Komissarov Gleb <gl.k@aol.com>
2022-03-10 16:38:02 +0300

    Deleted @Nullable :(

--- src/main/java/ru/mail/polis/test/glebkomissarov/MemorySegmentDaoFactory.java
+++ src/main/java/ru/mail/polis/test/glebkomissarov/MemorySegmentDaoFactory.java
@@ -1,8 +1,7 @@
 package ru.mail.polis.test.glebkomissarov;
 
 import jdk.incubator.foreign.MemorySegment;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
 import ru.mail.polis.BaseEntry;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
@@ -19,17 +18,17 @@ public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment
     }
 
     @Override
-    public String toString(@Nullable MemorySegment data) {
+    public String toString(MemorySegment data) {
         return data == null ? null : new String(data.toByteArray(), StandardCharsets.UTF_8);
     }
 
     @Override
-    public MemorySegment fromString(@Nullable String data) {
+    public MemorySegment fromString(String data) {
         return data == null ? null : MemorySegment.ofArray(data.getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
-    public BaseEntry<MemorySegment> fromBaseEntry(@NotNull Entry<MemorySegment> baseEntry) {
+    public BaseEntry<MemorySegment> fromBaseEntry(Entry<MemorySegment> baseEntry) {
         return new BaseEntry<>(baseEntry.key(), baseEntry.value());
     }
 }
