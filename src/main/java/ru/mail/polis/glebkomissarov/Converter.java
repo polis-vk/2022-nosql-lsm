18f0828735f60d98b407ca051d49aba85a1ecbbb
Komissarov Gleb <gl.k@aol.com>
2022-03-11 02:55:12 +0300

    Delete static

---
+++ src/main/java/ru/mail/polis/glebkomissarov/Converter.java
@@ -0,0 +1,94 @@
+package ru.mail.polis.glebkomissarov;
+
+import jdk.incubator.foreign.MemorySegment;
+import jdk.incubator.foreign.ResourceScope;
+import ru.mail.polis.BaseEntry;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Converter {
+
+    private Path pathToOffsets;
+    private Path pathToEntries;
+    private MemorySegment mappedSegmentEntries;
+    private MemorySegment mappedSegmentOffsets;
+    private long[] offsets;
+    private int idx;
+
+    public void startSerializeEntries(long dataCount, long fileSize, Path path) throws IOException {
+        if (dataCount == 0) {
+            return;
+        }
+
+        createPaths(path);
+        if (Files.notExists(pathToEntries)) {
+            Files.createFile(pathToEntries);
+            Files.createFile(pathToOffsets);
+        }
+
+        offsets = new long[(int) (dataCount * 2 + 1)];
+        offsets[0] = 0;
+
+        mappedSegmentEntries = newMapped(pathToEntries, fileSize);
+        mappedSegmentOffsets = newMapped(pathToOffsets, Long.SIZE * dataCount);
+    }
+
+    public BaseEntry<MemorySegment> searchEntry(Path path, MemorySegment key) throws IOException {
+        createPaths(path);
+
+        if (Files.notExists(pathToEntries)) {
+            return null;
+        }
+
+        mappedSegmentEntries = newMapped(pathToEntries, Files.size(pathToEntries));
+        mappedSegmentOffsets = newMapped(pathToOffsets, Files.size(pathToOffsets));
+
+        long[] offsets = mappedSegmentOffsets.toLongArray();
+
+        SegmentsComparator comparator = new SegmentsComparator();
+        MemorySegment currentKey;
+        long size;
+        for (int i = 0; i < offsets.length - 1; i += 2) {
+            size = offsets[i + 1] - offsets[i];
+            currentKey = mappedSegmentEntries.asSlice(offsets[i], size);
+            if (comparator.compare(key, currentKey) == 0) {
+                size = offsets[i + 2] - offsets[i + 1];
+                return new BaseEntry<>(currentKey, mappedSegmentEntries.asSlice(offsets[i + 1], size));
+            }
+        }
+        return null;
+    }
+
+    public void writeOffsets() {
+        mappedSegmentOffsets.asSlice(0L, (long) offsets.length * Long.SIZE / 8)
+                .copyFrom(MemorySegment.ofArray(offsets));
+    }
+
+    public void writeEntries(BaseEntry<MemorySegment> data, long keySize, long valueSize) {
+        offsets[idx + 1] = offsets[idx] + keySize;
+        offsets[idx + 2] = offsets[idx + 1] + valueSize;
+
+        mappedSegmentEntries.asSlice(offsets[idx], keySize).copyFrom(data.key());
+        mappedSegmentEntries.asSlice(offsets[idx + 1], valueSize).copyFrom(data.value());
+
+        idx += 2;
+    }
+
+    private void createPaths(Path path) {
+        pathToOffsets = path.resolve(String.valueOf(FileNames.OFFSETS.getName()));
+        pathToEntries = path.resolve(String.valueOf(FileNames.SAVED_DATA.getName()));
+    }
+
+    private MemorySegment newMapped(Path path, long size) throws IOException {
+        return MemorySegment.mapFile(
+                path,
+                0,
+                size,
+                FileChannel.MapMode.READ_WRITE,
+                ResourceScope.newConfinedScope()
+        );
+    }
+}
--- src/main/java/ru/mail/polis/test/glebkomissarov/MemorySegmentDaoFactory.java
+++ src/main/java/ru/mail/polis/test/glebkomissarov/MemorySegmentDaoFactory.java
@@ -1,9 +1,8 @@
 package ru.mail.polis.test.glebkomissarov;
 
 import jdk.incubator.foreign.MemorySegment;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import ru.mail.polis.BaseEntry;
+import ru.mail.polis.Config;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 import ru.mail.polis.glebkomissarov.MyMemoryDao;
@@ -14,22 +13,22 @@ import java.nio.charset.StandardCharsets;
 @DaoFactory(stage = 1, week = 2)
 public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment, BaseEntry<MemorySegment>> {
     @Override
-    public Dao<MemorySegment, BaseEntry<MemorySegment>> createDao() {
-        return new MyMemoryDao();
+    public Dao<MemorySegment, BaseEntry<MemorySegment>> createDao(Config config) {
+        return new MyMemoryDao(config);
     }
 
     @Override
-    public String toString(@Nullable MemorySegment data) {
+    public String toString(MemorySegment data) {
         return data == null ? null : new String(data.toByteArray(), StandardCharsets.UTF_8);
     }
 
     @Override
-    public MemorySegment fromString(@Nullable String data) {
+    public MemorySegment fromString (String data) {
         return data == null ? null : MemorySegment.ofArray(data.getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
-    public BaseEntry<MemorySegment> fromBaseEntry(@NotNull Entry<MemorySegment> baseEntry) {
+    public BaseEntry<MemorySegment> fromBaseEntry(Entry<MemorySegment> baseEntry) {
         return new BaseEntry<>(baseEntry.key(), baseEntry.value());
     }
 }
