29ce16a5d39da6db990be8924a893e50569e1b84
Artyom Trofimov <artem24630@gmail.com>
2022-03-12 20:28:07 +0300

    implement binary search in file

--- src/main/java/ru/mail/polis/artyomtrofimov/InMemoryDao.java
+++ src/main/java/ru/mail/polis/artyomtrofimov/InMemoryDao.java
@@ -14,17 +14,20 @@ import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<String, Entry<String>> {
-    private static final String FILENAME = "db.txt";
+    private static final String FILENAME = "db.dat";
+    private static final String INDEX_FILE_NAME = "index.ind";
     private final ConcurrentNavigableMap<String, Entry<String>> data = new ConcurrentSkipListMap<>();
     private final Path dataPath;
-    private long lastWritePos;
+    private final Path indexPath;
     private volatile boolean commit;
+
 
     public InMemoryDao(Config config) {
         if (config == null) {
             throw new IllegalArgumentException("Config shouldn't be null");
         }
         dataPath = config.basePath().resolve(FILENAME);
+        indexPath = config.basePath().resolve(INDEX_FILE_NAME);
     }
 
     @Override
@@ -57,19 +60,33 @@ public class InMemoryDao implements Dao<String, Entry<String>> {
     }
 
     private Entry<String> findInFileByKey(String key) throws IOException {
-        try (RandomAccessFile input = new RandomAccessFile(dataPath.toString(), "r")) {
+        try (RandomAccessFile input = new RandomAccessFile(dataPath.toString(), "r");
+             RandomAccessFile indexInput = new RandomAccessFile(indexPath.toString(), "r")) {
             input.seek(0);
             String line;
-            while (input.getFilePointer() <= input.length()) {
+            int size = input.readInt();
+            long left = 0;
+            long right = size;
+            long mid;
+            while (left < right) {
+                mid = left + (right - left) / 2;
+                indexInput.seek(mid * Long.BYTES);
+                input.seek(indexInput.readLong());
                 line = input.readUTF();
                 int delimiterIndex = line.indexOf(' ');
                 if (delimiterIndex == -1) {
                     continue;
                 }
-                int keyLength = Integer.parseInt(line, 0,delimiterIndex, 10);
-                String currentKey = line.substring(delimiterIndex + 1, delimiterIndex + keyLength + 1);
-                if (key.equals(currentKey)) {
-                    return new BaseEntry<>(currentKey, line.substring(delimiterIndex + keyLength + 1));
+                int keyLength = Integer.parseInt(line, 0, delimiterIndex, 10);
+                int entryDelimiter = delimiterIndex + keyLength + 1;
+                String currentKey = line.substring(delimiterIndex + 1, entryDelimiter);
+                int keyComparing = key.compareTo(currentKey);
+                if (keyComparing == 0) {
+                    return new BaseEntry<>(currentKey, line.substring(entryDelimiter));
+                } else if (keyComparing < 0) {
+                    right = mid;
+                } else {
+                    left = mid;
                 }
             }
             return null;
@@ -89,16 +106,31 @@ public class InMemoryDao implements Dao<String, Entry<String>> {
         if (commit) {
             return;
         }
-        try (RandomAccessFile output = new RandomAccessFile(dataPath.toString(), "rw")) {
-            output.seek(lastWritePos);
+        try (RandomAccessFile output = new RandomAccessFile(dataPath.toString(), "rw");
+             RandomAccessFile indexOut = new RandomAccessFile(indexPath.toString(), "rw")) {
+            output.seek(0);
             StringBuilder result = new StringBuilder();
+            output.writeInt(data.size());
             for (Entry<String> value : data.values()) {
                 result.append(value.key().length()).append(' ').append(value.key()).append(value.value());
+                indexOut.writeLong(output.getFilePointer());
                 output.writeUTF(result.toString());
                 result.setLength(0);
             }
-            lastWritePos = output.getFilePointer();
         }
         commit = true;
+    }
+
+    private static class EntryIterator implements Iterator<Entry<String>> {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Entry<String> next() {
+            return null;
+        }
     }
 }
