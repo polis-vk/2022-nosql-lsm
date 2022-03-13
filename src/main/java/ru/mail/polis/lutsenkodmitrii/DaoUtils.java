6ab6744880542c90935b8c40946318ae33b58a7f
LutsenkoDm <lutsenkodm@mail.ru>
2022-03-14 00:24:40 +0300

    Fix due to code climate

---
+++ src/main/java/ru/mail/polis/lutsenkodmitrii/DaoUtils.java
@@ -0,0 +1,150 @@
+package ru.mail.polis.lutsenkodmitrii;
+
+import ru.mail.polis.BaseEntry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class DaoUtils {
+
+    public static BaseEntry<String> readEntry(BufferedReader bufferedReader) throws IOException {
+        bufferedReader.skip(Integer.BYTES);
+        int keyLength = readUnsignedInt(bufferedReader);
+        if (keyLength == -1) {
+            return null;
+        }
+        char[] keyChars = new char[keyLength];
+        bufferedReader.read(keyChars);
+        return new BaseEntry<>(new String(keyChars), bufferedReader.readLine());
+    }
+
+    public static String readKey(BufferedReader bufferedReader) throws IOException {
+        char[] keyChars;
+        int keyLength;
+        keyLength = readUnsignedInt(bufferedReader);
+        keyChars = new char[keyLength];
+        bufferedReader.read(keyChars);
+        return new String(keyChars);
+    }
+
+    public static char[] intToCharArray(int k) {
+        char[] writeBuffer = new char[Integer.BYTES];
+        writeBuffer[0] = (char) (k >>> 24);
+        writeBuffer[1] = (char) (k >>> 16);
+        writeBuffer[2] = (char) (k >>> 8);
+        writeBuffer[3] = (char) (k >>> 0);
+        return writeBuffer;
+    }
+
+    public static int readUnsignedInt(BufferedReader bufferedReader) throws IOException {
+        int ch1 = bufferedReader.read();
+        int ch2 = bufferedReader.read();
+        int ch3 = bufferedReader.read();
+        int ch4 = bufferedReader.read();
+        if (ch1 == -1 || ch2 == -1 || ch3 == -1 || ch4 == -1) {
+            return -1;
+        }
+        return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
+    }
+
+    public static BaseEntry<String> ceilKey(Path path, BufferedReader bufferedReader, String key) throws IOException {
+        int keyLength;
+        int prevEntryLength;
+        char[] keyChars;
+        String currentKey;
+        long left = 0;
+        long right = Files.size(path) - Integer.BYTES;
+        long mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            bufferedReader.mark((int) right);
+            bufferedReader.skip(mid - left);
+            int readBytes = bufferedReader.readLine().length();
+            prevEntryLength = readUnsignedInt(bufferedReader);
+            if (mid + prevEntryLength >= right) {
+                bufferedReader.reset();
+                bufferedReader.skip(Integer.BYTES);
+                right = mid - prevEntryLength + readBytes + 1;
+            }
+            keyLength = readUnsignedInt(bufferedReader);
+            keyChars = new char[keyLength];
+            bufferedReader.read(keyChars);
+            currentKey = new String(keyChars);
+            String currentValue = bufferedReader.readLine();
+            int compareResult = key.compareTo(currentKey);
+            if (compareResult == 0) {
+                return new BaseEntry<>(currentKey, currentValue);
+            }
+            if (compareResult > 0) {
+                bufferedReader.mark(0);
+                bufferedReader.skip(Integer.BYTES);
+                keyLength = readUnsignedInt(bufferedReader);
+                if (keyLength == -1) {
+                    return null;
+                }
+                keyChars = new char[keyLength];
+                bufferedReader.read(keyChars);
+                String nextKey = new String(keyChars);
+                if (key.compareTo(nextKey) <= 0) {
+                    return new BaseEntry<>(nextKey, bufferedReader.readLine());
+                }
+                left = mid - prevEntryLength + readBytes;
+                bufferedReader.reset();
+            } else {
+                right = mid + readBytes;
+                bufferedReader.reset();
+            }
+        }
+        return null;
+    }
+
+    public static BaseEntry<String> searchInFile(Path path, String key) throws IOException {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path, UTF_8)) {
+            String fileMinKey = readKey(bufferedReader);
+            String fileMaxKey = readKey(bufferedReader);
+            if (key.compareTo(fileMinKey) < 0 || key.compareTo(fileMaxKey) > 0) {
+                return null;
+            }
+            int keyLength;
+            int prevEntryLength;
+            char[] keyChars;
+            String currentKey;
+            long left = 0;
+            long right = Files.size(path) - Integer.BYTES;
+            long mid;
+            while (left <= right) {
+                mid = (left + right) / 2;
+                bufferedReader.mark((int) right);
+                bufferedReader.skip(mid - left);
+                int readBytes = bufferedReader.readLine().length();
+                prevEntryLength = readUnsignedInt(bufferedReader);
+                if (mid + prevEntryLength >= right) {
+                    bufferedReader.reset();
+                    bufferedReader.skip(Integer.BYTES);
+                    right = mid - prevEntryLength + readBytes + 1;
+                }
+                keyLength = readUnsignedInt(bufferedReader);
+                keyChars = new char[keyLength];
+                bufferedReader.read(keyChars);
+                currentKey = new String(keyChars);
+                int compareResult = key.compareTo(currentKey);
+                if (compareResult > 0) {
+                    left = mid - prevEntryLength + readBytes;
+                } else if (compareResult < 0) {
+                    right = mid + readBytes;
+                    bufferedReader.reset();
+                } else {
+                    return new BaseEntry<>(currentKey, bufferedReader.readLine());
+                }
+            }
+            return null;
+        } catch (NoSuchFileException e) {
+            return null;
+        }
+    }
+}
--- src/main/java/ru/mail/polis/lutsenkodmitrii/InMemoryDao.java
+++ src/main/java/ru/mail/polis/lutsenkodmitrii/InMemoryDao.java
@@ -7,7 +7,11 @@ import ru.mail.polis.Dao;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.AbstractMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -15,6 +19,11 @@ import java.util.TreeMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static ru.mail.polis.lutsenkodmitrii.DaoUtils.ceilKey;
+import static ru.mail.polis.lutsenkodmitrii.DaoUtils.intToCharArray;
+import static ru.mail.polis.lutsenkodmitrii.DaoUtils.readEntry;
+import static ru.mail.polis.lutsenkodmitrii.DaoUtils.readKey;
+import static ru.mail.polis.lutsenkodmitrii.DaoUtils.searchInFile;
 
 public class InMemoryDao implements Dao<String, BaseEntry<String>> {
 
@@ -23,8 +32,8 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
     private static final OpenOption[] writeOptions = new OpenOption[]{
             StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE
     };
-    private static final String dataFileName = "daoData";
-    private static final String dataFileExtension = ".txt";
+    private static final String DATA_FILE_NAME = "daoData";
+    private static final String DATA_FILE_EXTENSION = ".txt";
     private final Config config;
     private final ConcurrentSkipListMap<String, BaseEntry<String>> data = new ConcurrentSkipListMap<>();
 
@@ -37,22 +46,20 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
     }
 
     class MergeIterator implements Iterator<BaseEntry<String>> {
-        private final TreeMap<Path, Map.Entry<BufferedReader, String>> filesReadMap = new TreeMap<>();
+        private final Map<Path, Map.Entry<BufferedReader, String>> filesReadMap = new TreeMap<>();
         private final ConcurrentSkipListMap<String, BaseEntry<String>> tempData = new ConcurrentSkipListMap<>();
         private final Iterator<BaseEntry<String>> inMemoryIterator;
-        private final String from;
         private final String to;
         private String inMemoryLastKey;
         boolean isFromNull;
         boolean isToNull;
 
         public MergeIterator(Config config, String from, String to) throws IOException {
-            this.from = from;
             this.to = to;
             isFromNull = from == null;
             isToNull = to == null;
             for (int i = 1; i < fileCounter; i++) {
-                Path path = config.basePath().resolve(dataFileName + i + dataFileExtension);
+                Path path = config.basePath().resolve(DATA_FILE_NAME + i + DATA_FILE_EXTENSION);
                 BufferedReader bufferedReader;
                 try {
                     bufferedReader = Files.newBufferedReader(path);
@@ -69,7 +76,10 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
                 }
                 if (firstEntry != null && (isToNull || to.compareTo(fileMinKey) >= 0)) {
                     tempData.put(firstEntry.key(), firstEntry);
-                    Map.Entry<BufferedReader, String> lastReadElem = new AbstractMap.SimpleEntry<>(bufferedReader, firstEntry.key());
+                    Map.Entry<BufferedReader, String> lastReadElem = new AbstractMap.SimpleEntry<>(
+                            bufferedReader,
+                            firstEntry.key()
+                    );
                     filesReadMap.put(path, lastReadElem);
                 }
             }
@@ -137,7 +147,7 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
             return data.get(key);
         }
         for (int i = fileCounter - 1; i >= 1; i--) {
-            Path path = config.basePath().resolve(dataFileName + i + dataFileExtension);
+            Path path = config.basePath().resolve(DATA_FILE_NAME + i + DATA_FILE_EXTENSION);
             BaseEntry<String> entry = searchInFile(path, key);
             if (entry != null) {
                 return entry;
@@ -156,7 +166,7 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
         if (data.isEmpty() || config == null) {
             return;
         }
-        Path dataFilePath = config.basePath().resolve(dataFileName + fileCounter++ + dataFileExtension);
+        Path dataFilePath = generateNextFilePath();
         try (BufferedWriter bufferedFileWriter = Files.newBufferedWriter(dataFilePath, UTF_8, writeOptions)) {
             int bufferedEntriesCounter = 0;
             String fileMinKey = data.firstKey();
@@ -170,7 +180,9 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
                 bufferedFileWriter.write(intToCharArray(baseEntry.key().length()));
                 bufferedFileWriter.write(baseEntry.key());
                 bufferedFileWriter.write(baseEntry.value() + '\n');
-                bufferedFileWriter.write(intToCharArray(Integer.BYTES + Integer.BYTES + baseEntry.key().length() + baseEntry.value().length() + 1));
+                bufferedFileWriter.write(intToCharArray(
+                        Integer.BYTES + Integer.BYTES
+                                + baseEntry.key().length() + baseEntry.value().length() + 1));
                 bufferedEntriesCounter++;
                 if (bufferedEntriesCounter % BUFFER_FLUSH_LIMIT == 0) {
                     bufferedFileWriter.flush();
@@ -193,140 +205,7 @@ public class InMemoryDao implements Dao<String, BaseEntry<String>> {
         return data.subMap(from, true, to, false).values().iterator();
     }
 
-    private static BaseEntry<String> ceilKey(Path path, BufferedReader bufferedReader, String key) throws IOException {
-        int keyLength;
-        int prevEntryLength;
-        char[] keyChars;
-        String currentKey;
-        long left = 0;
-        long right = Files.size(path) - Integer.BYTES;
-        long mid;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            bufferedReader.mark((int) right);
-            bufferedReader.skip(mid - left);
-            int readBytes = bufferedReader.readLine().length();
-            prevEntryLength = readUnsignedInt(bufferedReader);
-            if (mid + prevEntryLength >= right) {
-                bufferedReader.reset();
-                bufferedReader.skip(Integer.BYTES);
-                right = mid - prevEntryLength + readBytes + 1;
-            }
-            keyLength = readUnsignedInt(bufferedReader);
-            keyChars = new char[keyLength];
-            bufferedReader.read(keyChars);
-            currentKey = new String(keyChars);
-            String currentValue = bufferedReader.readLine();
-            int compareResult = key.compareTo(currentKey);
-            if (compareResult == 0) {
-                return new BaseEntry<>(currentKey, currentValue);
-            }
-            if (compareResult > 0) {
-                bufferedReader.mark(0);
-                bufferedReader.skip(Integer.BYTES);
-                keyLength = readUnsignedInt(bufferedReader);
-                if (keyLength == -1) {
-                    return null;
-                }
-                keyChars = new char[keyLength];
-                bufferedReader.read(keyChars);
-                String nextKey = new String(keyChars);
-                if (key.compareTo(nextKey) <= 0) {
-                    return new BaseEntry<>(nextKey, bufferedReader.readLine());
-                }
-                left = mid - prevEntryLength + readBytes;
-                bufferedReader.reset();
-            } else {
-                right = mid + readBytes;
-                bufferedReader.reset();
-            }
-        }
-        return null;
+    private Path generateNextFilePath() {
+        return config.basePath().resolve(DATA_FILE_NAME + fileCounter++ + DATA_FILE_EXTENSION);
     }
-
-    private static BaseEntry<String> readEntry(BufferedReader bufferedReader) throws IOException {
-        bufferedReader.skip(Integer.BYTES);
-        int keyLength = readUnsignedInt(bufferedReader);
-        if (keyLength == -1) {
-            return null;
-        }
-        char[] keyChars = new char[keyLength];
-        bufferedReader.read(keyChars);
-        return new BaseEntry<>(new String(keyChars), bufferedReader.readLine());
-    }
-
-    private static String readKey(BufferedReader bufferedReader) throws IOException {
-        char[] keyChars;
-        int keyLength;
-        keyLength = readUnsignedInt(bufferedReader);
-        keyChars = new char[keyLength];
-        bufferedReader.read(keyChars);
-        return new String(keyChars);
-    }
-
-    private static char[] intToCharArray(int k) {
-        char[] writeBuffer = new char[Integer.BYTES];
-        writeBuffer[0] = (char) (k >>> 24);
-        writeBuffer[1] = (char) (k >>> 16);
-        writeBuffer[2] = (char) (k >>> 8);
-        writeBuffer[3] = (char) (k >>> 0);
-        return writeBuffer;
-    }
-
-    public static int readUnsignedInt(BufferedReader bufferedReader) throws IOException {
-        int ch1 = bufferedReader.read();
-        int ch2 = bufferedReader.read();
-        int ch3 = bufferedReader.read();
-        int ch4 = bufferedReader.read();
-        if (ch1 == -1 || ch2 == -1 || ch3 == -1 || ch4 == -1) {
-            return -1;
-        }
-        return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
-    }
-
-    private static BaseEntry<String> searchInFile(Path path, String key) throws IOException {
-        try (BufferedReader bufferedReader = Files.newBufferedReader(path, UTF_8)) {
-            String fileMinKey = readKey(bufferedReader);
-            String fileMaxKey = readKey(bufferedReader);
-            if (key.compareTo(fileMinKey) < 0 || key.compareTo(fileMaxKey) > 0) {
-                return null;
-            }
-            int keyLength;
-            int prevEntryLength;
-            char[] keyChars;
-            String currentKey;
-            long left = 0;
-            long right = Files.size(path) - Integer.BYTES;
-            long mid;
-            while (left <= right) {
-                mid = (left + right) / 2;
-                bufferedReader.mark((int) right);
-                bufferedReader.skip(mid - left);
-                int readBytes = bufferedReader.readLine().length();
-                prevEntryLength = readUnsignedInt(bufferedReader);
-                if (mid + prevEntryLength >= right) {
-                    bufferedReader.reset();
-                    bufferedReader.skip(Integer.BYTES);
-                    right = mid - prevEntryLength + readBytes + 1;
-                }
-                keyLength = readUnsignedInt(bufferedReader);
-                keyChars = new char[keyLength];
-                bufferedReader.read(keyChars);
-                currentKey = new String(keyChars);
-                int compareResult = key.compareTo(currentKey);
-                if (compareResult > 0) {
-                    left = mid - prevEntryLength + readBytes;
-                } else if (compareResult < 0) {
-                    right = mid + readBytes;
-                    bufferedReader.reset();
-                } else {
-                    return new BaseEntry<>(currentKey, bufferedReader.readLine());
-                }
-            }
-            return null;
-        } catch (NoSuchFileException e) {
-            return null;
-        }
-    }
-
 }
