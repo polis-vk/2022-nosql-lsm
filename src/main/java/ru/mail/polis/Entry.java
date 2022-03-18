5a56c3725fb37edae346bd6b9364fb6752da40c1
mariohuq <mariohuq@github.com>
2022-03-18 20:59:22 +0300

    Add annotations dependency, try to use

--- src/main/java/ru/mail/polis/Entry.java
+++ src/main/java/ru/mail/polis/Entry.java
@@ -1,7 +1,12 @@
 package ru.mail.polis;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public interface Entry<D> {
+    @NotNull
     D key();
 
+    @Nullable
     D value();
 }
