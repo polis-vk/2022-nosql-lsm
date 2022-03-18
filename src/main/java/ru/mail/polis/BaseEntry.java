e3b218ea000b32a4acff6ba316d166d808cd96a9
mariohuq <mariohuq@github.com>
2022-03-18 21:34:07 +0300

    * NotNull BaseEntry::key * Nullable BaseEntry::value * NotNull Config::basePath, * Nullable Dao::get(from, to) args * Nullable Dao::get(key) return value

--- src/main/java/ru/mail/polis/BaseEntry.java
+++ src/main/java/ru/mail/polis/BaseEntry.java
@@ -1,6 +1,9 @@
 package ru.mail.polis;
 
-public record BaseEntry<Data>(Data key, Data value) implements Entry<Data> {
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public record BaseEntry<Data>(@NotNull Data key, @Nullable Data value) implements Entry<Data> {
     @Override
     public String toString() {
         return "{" + key + ":" + value + "}";
--- src/main/java/ru/mail/polis/Config.java
+++ src/main/java/ru/mail/polis/Config.java
@@ -1,6 +1,8 @@
 package ru.mail.polis;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.nio.file.Path;
 
-public record Config(Path basePath) {
+public record Config(@NotNull Path basePath) {
 }
