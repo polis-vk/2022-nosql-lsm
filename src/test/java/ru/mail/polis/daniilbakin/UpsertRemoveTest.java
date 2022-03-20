428f54ec404b69a0ead183fbc6c37dea0dc1c722
done_bd <dinil.bd@mail.ru>
2022-03-20 12:10:43 +0300

    Add test which broke my realization

--- src/test/java/ru/mail/polis/daniilbakin/UpsertRemoveTest.java
+++ src/test/java/ru/mail/polis/daniilbakin/UpsertRemoveTest.java
@@ -1,6 +1,7 @@
 package ru.mail.polis.daniilbakin;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import ru.mail.polis.BaseEntry;
 import ru.mail.polis.BaseTest;
@@ -69,6 +70,45 @@ public class UpsertRemoveTest extends BaseTest {
         dao.upsert(entryAt(2));
 
         assertSame(dao.all(), entryAt(1), entryAt(2));
+    }
+
+    @DaoTest(stage = 3)
+    void manyRemoveRecords(Dao<String, Entry<String>> dao) throws IOException {
+        for (int i = 0; i < 6; i++) {
+            dao.upsert(entryAt(i));
+        }
+        dao.close();
+
+        dao = DaoFactory.Factory.reopen(dao);
+        dao.upsert(new BaseEntry<>(keyAt(1), null));
+        dao.upsert(new BaseEntry<>(keyAt(2), null));
+        dao.upsert(new BaseEntry<>(keyAt(4), null));
+        dao.upsert(new BaseEntry<>(keyAt(5), null));
+        dao.close();
+
+        dao = DaoFactory.Factory.reopen(dao);
+        dao.upsert(new BaseEntry<>(keyAt(3), null));
+        dao.upsert(entryAt(4));
+        dao.upsert(new BaseEntry<>(keyAt(5), "new value"));
+        dao.close();
+
+        dao = DaoFactory.Factory.reopen(dao);
+        dao.upsert(entryAt(2));
+        dao.upsert(entryAt(1));
+
+        for (Iterator<Entry<String>> it = dao.all(); it.hasNext(); ) {
+            Entry<String> entry = it.next();
+            System.out.println(entry);
+        }
+
+        assertSame(
+                dao.all(),
+                entryAt(0),
+                entryAt(1),
+                entryAt(2),
+                entryAt(4),
+                new BaseEntry<>(keyAt(5), "new value")
+        );
     }
 
 }
