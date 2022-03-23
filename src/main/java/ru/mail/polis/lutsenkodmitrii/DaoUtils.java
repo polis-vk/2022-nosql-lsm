a001aac4691895d8484a914576c165567cd7326f
LutsenkoDm <lutsenkodm@mail.ru>
2022-03-23 17:48:34 +0300

    Fixe due to comments. Add delete opportunity.

--- src/main/java/ru/mail/polis/lutsenkodmitrii/DaoUtils.java
+++ src/main/java/ru/mail/polis/lutsenkodmitrii/DaoUtils.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static ru.mail.polis.lutsenkodmitrii.PersistenceRangeDao.NEXT_LINE_IN_STRING;
+import static ru.mail.polis.lutsenkodmitrii.PersistenceRangeDao.NEXT_LINE_REPLACEMENT;
+
 public final class DaoUtils {
 
     public static final int CHARS_IN_INT = Integer.SIZE / Character.SIZE;
@@ -16,9 +19,8 @@ public final class DaoUtils {
     }
 
     public static void writeUnsignedInt(int k, BufferedWriter bufferedWriter) throws IOException {
-        // Почему здесь +1 См. метод ниже - readUnsignedInt(BufferedReader bufferedReader)
-        bufferedWriter.write((char) ((k + 1) >>> 16));
-        bufferedWriter.write((char) (k + 1));
+        bufferedWriter.write((k + '0') >>> 16);
+        bufferedWriter.write((k + '0'));
     }
 
     public static int readUnsignedInt(BufferedReader bufferedReader) throws IOException {
@@ -30,20 +32,7 @@ public final class DaoUtils {
         if (ch1 == -1 || ch2 == -1) {
             return -1;
         }
-        // Здесь -1 делается по следующей причине:
-        // В MusicTest все ключи имеют длину 13, кроме одного, с длинной 14
-        // Если не добавлять в методе выше (writeUnsignedInt) 1 при записи, там k + 1 везде, то:
-        // При записи в файл длины ключа для "Ar1", "Al12", "T1111" длины 14 в файл его длина записывается как бы 2 раза
-        // С остальными ключамии (длина 13) все нормально
-        // Однако, если при записи добавить 1, как следствии в строке ниже надо вычитать её, то все нормально
-        // То есть на месте ключей длины 13 будет 14, а при 14 будет 15, это в файле
-        // При чтении 1 вычитается
-        // Я подумад сначала что может быть при char 14 какой-то особый символ или еще что-то такое
-        // Но ведь добавив 1 все ключи 13 записываются как 14 и записываеются нормально.
-        // Получается если записывать 13 + 1 (+1 внутри метода writeUnsignedInt) , то все нормально
-        // А если 14 без добавления +1 то не нормально
-        // Почему так мне не понятно, единственна догадка - приведения типов или что-то подобное, вызываемое этой 1
-        return (ch1 << 16) + (ch2) - 1;
+        return (ch1 << 16) + ch2 - '0';
     }
 
     public static String readKey(BufferedReader bufferedReader) throws IOException {
@@ -53,28 +42,28 @@ public final class DaoUtils {
         }
         char[] keyChars = new char[keyLength];
         bufferedReader.read(keyChars);
-        return new String(keyChars);
+        return postprocess(new String(keyChars));
+    }
+
+    public static String readValue(BufferedReader bufferedReader) throws IOException {
+        return bufferedReader.read() == PersistenceRangeDao.EXISTING_MARK
+                ? postprocess(bufferedReader.readLine())
+                : null;
     }
 
     public static BaseEntry<String> readEntry(BufferedReader bufferedReader) throws IOException {
-        bufferedReader.skip(CHARS_IN_INT); // Пропускаем длину предыдущей записи и всего что к ней относиться в байтах
-        int keyLength = readUnsignedInt(bufferedReader);
-        // Для чтения везде используется BufferedReader.
-        // Все его методы возвращают -1, когда достигнут конец файла
-        // Для поддержания идентичного контракта в методе readUnsignedInt возвращается -1 если достигнут конец файла
-        // И как следствие возвращается null в текущем методе
-        // Таким образом выполняется и проверка конца файла, поэтому не кидается EOFExсeption
-        // Более изящный способ проверить конец файла для BufferedReader я не нашел/придумал
-        // На skip, который выше нельзя проверять так как там идет запись длины предыдущей записи
-        // И на последней записи можно успешно сделать skip, но ключа уже не будет
-        // Можно для последней записи не записывать длину, но код тогда будет некрасивый
-        // Или надо будет записать и сразу удалять эту длину, от чего я тоже отказался
-        if (keyLength == -1) {
+        bufferedReader.skip(CHARS_IN_INT); // Пропускаем длину предыдущей записи
+        String key = readKey(bufferedReader);
+        if (key == null) {
+            // Таким образом выполняется проверка конца файла, поэтому не кидается EOFExсeption
+            // Более изящный способ проверить конец файла для BufferedReader я не нашел/придумал
+            // На skip, который выше нельзя проверять так как там идет запись длины предыдущей записи
+            // И на последней записи можно успешно сделать skip, но ключа уже не будет
+            // Можно для последней записи не записывать длину, но код тогда будет некрасивый
+            // Или надо будет записать и сразу удалять эту длину, от чего я тоже отказался
             return null;
         }
-        char[] keyChars = new char[keyLength];
-        bufferedReader.read(keyChars);
-        return new BaseEntry<>(new String(keyChars), bufferedReader.readLine());
+        return new BaseEntry<>(key, readValue(bufferedReader));
     }
 
     /**
@@ -82,70 +71,87 @@ public final class DaoUtils {
      * Везде, где упоминается размер / длина, имеется в виду относительно char, а не байтов.
      * left - левая граница, равная offset - сдвиг относительно начала файла
      * offset нужен, чтобы пропустить к примеру минимальный и максимальный ключи в начале (См. Описание формата файла)
-     * right - правая граница равная размеру файла минус размер числа, которое означает длину относящегося предыдущей записи
+     * right - правая граница равная размеру файла минус размер числа,
+     * которое означает длину относящегося предыдущей записи
      * Минусуем, чтобы гарантированно читать это число целиком.
-     * mid - середина по размеру между left и right, (left + right) / 2;
-     * mid указывает на ту позицию относительно начала строки, на какую повезет, необязательно на начало
-     * Но реализован гарантированный переход на начало следующей строки, для этого делается readline,
-     * Каждое entry начинается с новой строки ('\n' в исходном ключе / значении экранируется)
+     * position - середина по размеру между left и right, (left + right) / 2;
+     * position после операции выше указывает на ту позицию относительно начала строки, на какую повезет,
+     * необязательно на начало
+     * Поэтому реализован гарантированный переход на начало следующей строки, для этого делается readline,
+     * Каждое entry начинается с новой строки ('\n' в исходном ключе и значении экранируется)
      * Начало строки начинается всегда с размера прошлой строки, то есть прошлой entry
-     * плюс размера одного int(этого же число, но напрошлой строке)
+     * плюс размера одного int(этого же число, но на прошлой строке)
      * При этом left всегда указывает на начало строки, а right на конец (речь про разные строки / entry)
-     * Перед тем как переходить на mid всегда ставиться метка в позиции left, то есть в начале строки
+     * Перед тем как переходить на position в середину, всегда ставиться метка в позиции left, то есть в начале строки
      * Всегда идет проверка на случай если мы пополи на середину последней строки :
-     * mid + прочитанные байты за readline == right - offset,
+     * position + readBytes(прочитанные байты с помощью readline) == right - offset,
      * вычитаем offset так как right относительно всего размера файла,
-     * Если равенство выполняется, то возвращаемся в конец последней строки и идет обычная обработка :
+     * Если равенство выполняется, то возвращаемся в конец последней строки -
+     * position ставим в left + CHARS_IN_INT (длина числа, размера предыдущей строки), readBytes обнуляем,
+     * дальше идет обычная обработка :
      * Читаем ключ, значение (все равно придется его читать чтобы дойти до след ключа),
      * сравниваем ключ, если он равен, то return
      * Если текущий ключ меньше заданного, то читаем следующий
      * Если следующего нет, то return null, так ищем границу сверху, а последний ключ меньше заданного
      * Если этот следующий ключ меньше или равен заданному, то читаем его value и return
      * В зависимости от результата сравнения left и right устанавливаем в начало или конец рассматриваемого ключа
-     * В итоге идея следующая найти пару ключей, между которыми лежит исходный и вернуть второй или равный исходному
+     * mark делается всегда в начале entry то есть в позиции left. В первом случае чтобы вернуться, как бы skip наоборот
+     * Во втором чтобы сделать peek для nextKey и была возможность просмотреть его и вернуться в его начало.
+     * В итоге идея следующая найти пару ключей, между которыми лежит исходный и вернуть второй или равный исходному,
+     * при этом не храня индексы для сдвигов ключей вовсе
      */
     public static BaseEntry<String> ceilKey(Path path, BufferedReader bufferedReader,
                                             String key, long offset) throws IOException {
-
         int prevEntryLength;
         String currentKey;
         long left = offset;
         long right = Files.size(path) - CHARS_IN_INT;
-        long mid;
+        long position;
         while (left <= right) {
-            mid = (left + right) / 2;
+            position = (left + right) / 2;
             bufferedReader.mark((int) right);
-            bufferedReader.skip(mid - left);
-            int readBytes = bufferedReader.readLine().length() + 1;
+            bufferedReader.skip(position - left);
+            String leastPartOfLine = bufferedReader.readLine();
+            int readBytes = leastPartOfLine.length() + 1;
             prevEntryLength = readUnsignedInt(bufferedReader);
-            if (mid + readBytes >= right - offset) {
+            if (position + readBytes >= right) {
                 bufferedReader.reset();
                 bufferedReader.skip(CHARS_IN_INT);
-                right = mid - prevEntryLength + readBytes + 1;
+                right = position - prevEntryLength + readBytes + 1;
+                position = left + CHARS_IN_INT;
+                readBytes = 0;
             }
             currentKey = readKey(bufferedReader);
-            String currentValue = bufferedReader.readLine();
+            String currentValue = readValue(bufferedReader);
             int compareResult = key.compareTo(currentKey);
             if (compareResult == 0) {
                 return new BaseEntry<>(currentKey, currentValue);
             }
             if (compareResult > 0) {
                 bufferedReader.mark(0);
-                bufferedReader.skip(CHARS_IN_INT);
+                prevEntryLength = readUnsignedInt(bufferedReader);
                 String nextKey = readKey(bufferedReader);
                 if (nextKey == null) {
                     return null;
                 }
                 if (key.compareTo(nextKey) <= 0) {
-                    return new BaseEntry<>(nextKey, bufferedReader.readLine());
+                    return new BaseEntry<>(nextKey, readValue(bufferedReader));
                 }
-                left = mid - prevEntryLength + readBytes;
+                left = position + readBytes + prevEntryLength;
                 bufferedReader.reset();
             } else {
-                right = mid + readBytes;
+                right = position + readBytes;
                 bufferedReader.reset();
             }
         }
         return null;
+    }
+
+    public static String preprocess(String str) {
+        return str.replace(NEXT_LINE_IN_STRING, NEXT_LINE_REPLACEMENT);
+    }
+
+    public static String postprocess(String str) {
+        return str.replace(NEXT_LINE_REPLACEMENT, NEXT_LINE_IN_STRING);
     }
 }
--- src/main/java/ru/mail/polis/lutsenkodmitrii/PersistenceRangeDao.java
+++ src/main/java/ru/mail/polis/lutsenkodmitrii/PersistenceRangeDao.java
@@ -13,8 +13,10 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static ru.mail.polis.lutsenkodmitrii.DaoUtils.preprocess;
 
 /**
  * ----------------------------------------------------------------------------------------------*
@@ -28,7 +30,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  * - Ключ
  * - Значение + '\n'
  * - Длина всего записанного + размер самого числа относительно типа char
- *
+ * <p>
  * Пример (пробелы и переносы строк для наглядности):
  * k2 k55
  * 0 2 k2 v2 '\n'
@@ -42,6 +44,13 @@ public class PersistenceRangeDao implements Dao<String, BaseEntry<String>> {
     private static final OpenOption[] writeOptions = new OpenOption[]{
             StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE
     };
+    public static final int DELETED_MARK = 0;
+    public static final int EXISTING_MARK = 1;
+    public static final char NEXT_LINE = '\n';
+    public static final String NEXT_LINE_REPLACEMENT = "\\n";
+    public static final String NEXT_LINE_IN_STRING = String.valueOf(NEXT_LINE);
+    public static final int NEXT_LINE_REPLACEMENT_ADDITION =
+            NEXT_LINE_REPLACEMENT.length() - NEXT_LINE_IN_STRING.length();
     public static final String DATA_FILE_NAME = "daoData";
     public static final String DATA_FILE_EXTENSION = ".txt";
     private final ConcurrentSkipListMap<String, BaseEntry<String>> data = new ConcurrentSkipListMap<>();
@@ -50,9 +59,9 @@ public class PersistenceRangeDao implements Dao<String, BaseEntry<String>> {
 
     public PersistenceRangeDao(Config config) throws IOException {
         this.config = config;
-        try {
-            currentFileNumber = Files.find(config.basePath(), 1,
-                    (p, a) -> a.isRegularFile() && p.getFileName().toString().endsWith(DATA_FILE_EXTENSION)).count();
+        try (Stream<Path> stream = Files.find(config.basePath(), 1,
+                (p, a) -> a.isRegularFile() && p.getFileName().toString().endsWith(DATA_FILE_EXTENSION))) {
+            currentFileNumber = stream.count();
         } catch (NoSuchFileException e) {
             currentFileNumber = 0;
         }
@@ -75,22 +84,35 @@ public class PersistenceRangeDao implements Dao<String, BaseEntry<String>> {
         }
         Path dataFilePath = generateNextFilePath();
         try (BufferedWriter bufferedFileWriter = Files.newBufferedWriter(dataFilePath, UTF_8, writeOptions)) {
-            String fileMinKey = data.firstKey();
-            String fileMaxKey = data.lastKey();
+            String fileMinKey = preprocess(data.firstKey());
+            String fileMaxKey = preprocess(data.lastKey());
             DaoUtils.writeUnsignedInt(fileMinKey.length(), bufferedFileWriter);
             bufferedFileWriter.write(fileMinKey);
             DaoUtils.writeUnsignedInt(fileMaxKey.length(), bufferedFileWriter);
             bufferedFileWriter.write(fileMaxKey);
             DaoUtils.writeUnsignedInt(0, bufferedFileWriter);
+            int keyNextLines;
+            int valueNextLines;
             for (BaseEntry<String> baseEntry : data.values()) {
-                DaoUtils.writeUnsignedInt(baseEntry.key().length(), bufferedFileWriter);
-                bufferedFileWriter.write(baseEntry.key());
-                // Длина value не пишется так как она не нужна
-                bufferedFileWriter.write(baseEntry.value() + '\n');
-                DaoUtils.writeUnsignedInt(
-                        DaoUtils.CHARS_IN_INT + DaoUtils.CHARS_IN_INT
-                                + baseEntry.key().length() + baseEntry.value().length() + 1,
+                keyNextLines = (int) baseEntry.key().chars().parallel().filter(ch -> ch == NEXT_LINE).count();
+                DaoUtils.writeUnsignedInt(baseEntry.key().length() + keyNextLines * NEXT_LINE_REPLACEMENT_ADDITION,
                         bufferedFileWriter);
+                bufferedFileWriter.write(keyNextLines == 0 ? baseEntry.key() : preprocess(baseEntry.key()));
+                if (baseEntry.value() == null) {
+                    bufferedFileWriter.write(DELETED_MARK + NEXT_LINE);
+                    DaoUtils.writeUnsignedInt(DaoUtils.CHARS_IN_INT + DaoUtils.CHARS_IN_INT
+                            + baseEntry.key().length() + keyNextLines * NEXT_LINE_REPLACEMENT_ADDITION
+                            + NEXT_LINE_IN_STRING.length(), bufferedFileWriter);
+                    continue;
+                }
+                bufferedFileWriter.write(EXISTING_MARK);
+                valueNextLines = (int) baseEntry.value().chars().parallel().filter(ch -> ch == NEXT_LINE).count();
+                bufferedFileWriter.write((valueNextLines == 0 ? baseEntry.value() : preprocess(baseEntry.value()))
+                        + NEXT_LINE); // Длина value не пишется так как она не нужна
+                DaoUtils.writeUnsignedInt(DaoUtils.CHARS_IN_INT + DaoUtils.CHARS_IN_INT
+                        + baseEntry.key().length() + baseEntry.value().length()
+                        + (keyNextLines + valueNextLines) * NEXT_LINE_REPLACEMENT_ADDITION
+                        + NEXT_LINE_IN_STRING.length(), bufferedFileWriter);
             }
             bufferedFileWriter.flush();
         }
