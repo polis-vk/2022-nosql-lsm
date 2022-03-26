ece0d4deedfa472db79098e85c17a05faf64c921
Artyom Trofimov <a.t.0202@bk.ru>
2022-03-26 10:28:56 +0300

    fix codestyle

--- src/main/java/ru/mail/polis/artyomtrofimov/FileIterator.java
+++ src/main/java/ru/mail/polis/artyomtrofimov/FileIterator.java
@@ -11,7 +11,7 @@ import java.util.NoSuchElementException;
 
 public class FileIterator implements Iterator<Entry<String>> {
     private final String to;
-    private RandomAccessFile raf;
+    private final RandomAccessFile raf;
     private Entry<String> nextEntry;
     private final long fileLength;
 
