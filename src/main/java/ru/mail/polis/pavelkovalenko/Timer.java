53cbffab717f4e24e659d044520cc0db46ec9f4f
PaaavelZ <paaavel.2024@gmail.com>
2022-03-27 07:31:02 +0300

    Small fixes

--- src/main/java/ru/mail/polis/pavelkovalenko/Timer.java
+++ src/main/java/ru/mail/polis/pavelkovalenko/Timer.java
@@ -1,22 +1,60 @@
 package ru.mail.polis.pavelkovalenko;
 
-import java.time.Clock;
-
 public class Timer {
 
-    private final Clock clock = Clock.systemDefaultZone();
     private long startTime;
 
     public Timer() {
-        this.startTime = clock.millis();
+        this.startTime = System.nanoTime();
     }
 
-    public double elapse() {
-        return (double)(clock.millis() - startTime) / 1000;
+    private enum Time {
+        SECONDS(0, "s"),
+        MILLISECONDS(3, "ms"),
+        MICROSECONDS(6, "mcs"),
+        NANOSECONDS(9, "ns");
+
+        private final double multiplier;
+        private final double divider;
+        private final String system;
+
+        Time(double factor, String system) {
+            this.multiplier = Math.pow(10, factor);
+            this.divider = Math.pow(10, -factor);
+            this.system = system;
+        }
+
+        public double getMultiplier() {
+            return multiplier;
+        }
+
+        public double getDivider() {
+            return divider;
+        }
+
+        public String getSystem() {
+            return system;
+        }
+    }
+
+    public String elapse() {
+        double result = (System.nanoTime() - startTime) / Math.pow(10, 9);
+        long times;
+        StringBuilder resultStr = new StringBuilder();
+
+        for (Time time: Time.values()) {
+            if (Double.compare(result * time.getMultiplier(), 0) > 0) {
+                times = (long)Math.floor(result * time.getMultiplier());
+                result -= times * time.getDivider();
+                resultStr.append(times).append(time.getSystem()).append(":");
+            }
+        }
+
+        return resultStr.toString();
     }
 
     public void refresh() {
-        startTime = clock.millis();
+        startTime = System.nanoTime();
     }
 
 }
