b40d35fe8f109ec314893c65b4f661a71d627339
PaaavelZ <paaavel.2024@gmail.com>
2022-03-27 07:45:42 +0300

    Add timer

---
+++ src/main/java/ru/mail/polis/Timer.java
@@ -0,0 +1,60 @@
+package ru.mail.polis;
+
+public class Timer {
+
+    private long startTime;
+
+    public Timer() {
+        this.startTime = System.nanoTime();
+    }
+
+    public String elapse() {
+        double result = (System.nanoTime() - startTime) / Math.pow(10, 9);
+        long times;
+        StringBuilder resultStr = new StringBuilder();
+
+        for (Time time: Time.values()) {
+            if (Double.compare(result * time.getMultiplier(), 0) > 0) {
+                times = (long)Math.floor(result * time.getMultiplier());
+                result -= times * time.getDivider();
+                resultStr.append(times).append(time.getSystem()).append(":");
+            }
+        }
+
+        return resultStr.toString();
+    }
+
+    public void refresh() {
+        startTime = System.nanoTime();
+    }
+
+    private enum Time {
+        SECONDS(0, "s"),
+        MILLISECONDS(3, "ms"),
+        MICROSECONDS(6, "mcs"),
+        NANOSECONDS(9, "ns");
+
+        private final double multiplier;
+        private final double divider;
+        private final String system;
+
+        Time(double factor, String system) {
+            this.multiplier = Math.pow(10, factor);
+            this.divider = Math.pow(10, -factor);
+            this.system = system;
+        }
+
+        public double getMultiplier() {
+            return multiplier;
+        }
+
+        public double getDivider() {
+            return divider;
+        }
+
+        public String getSystem() {
+            return system;
+        }
+    }
+
+}
