030d4821e96cb4878f11f77704448dbe3bf5bc28
mariohuq <mariohuq@github.com>
2022-03-31 21:02:30 +0300

    week 3

--- src/main/java/ru/mail/polis/test/dmitrykondraev/MemorySegmentDaoFactory.java
+++ src/main/java/ru/mail/polis/test/dmitrykondraev/MemorySegmentDaoFactory.java
@@ -14,7 +14,7 @@ import java.io.IOException;
  * Author: Dmitry Kondraev.
  */
 
-@DaoFactory(stage = 3, week = 2)
+@DaoFactory(stage = 3, week = 3)
 public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment, MemorySegmentEntry> {
 
     @Override
