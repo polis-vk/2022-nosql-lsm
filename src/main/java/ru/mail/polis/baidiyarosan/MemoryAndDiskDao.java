78963e200acc54a92820a280827b9f84dd0102eb
AirBurn <rosan1233456@gmail.com>
2022-04-03 21:21:55 +0300

    damn windows

--- src/main/java/ru/mail/polis/baidiyarosan/MemoryAndDiskDao.java
+++ src/main/java/ru/mail/polis/baidiyarosan/MemoryAndDiskDao.java
@@ -87,9 +87,6 @@ public class MemoryAndDiskDao implements Dao<ByteBuffer, BaseEntry<ByteBuffer>> 
 
     @Override
     public void compact() throws IOException {
-        if(collection.isEmpty() && filesCount == 1) {
-            return;
-        }
         Iterator<BaseEntry<ByteBuffer>> iter = get(null, null);
         collection.clear();
         FileUtils.compact(iter, path);
