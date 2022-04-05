baefbcaed129585841058346ca9ade9a4de0e2fc
Денис Жидков <denis.zhidkov01@gmail.com>
2022-04-05 18:52:16 +0300

    Almost done compact(), need to add check for end of writing tmp files, trying to resolve writeUTF() trouble

--- src/main/java/ru/mail/polis/deniszhidkov/DaoWriter.java
+++ src/main/java/ru/mail/polis/deniszhidkov/DaoWriter.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Iterator;
 import java.util.Map;
 
 public class DaoWriter {
@@ -26,14 +27,16 @@ public class DaoWriter {
                         Files.newOutputStream(
                                 pathToDataFile,
                                 StandardOpenOption.CREATE,
-                                StandardOpenOption.WRITE
+                                StandardOpenOption.WRITE,
+                                StandardOpenOption.TRUNCATE_EXISTING
                         )));
              DataOutputStream offsetsWriter = new DataOutputStream(
                      new BufferedOutputStream(
                              Files.newOutputStream(
                                      pathToOffsetsFile,
                                      StandardOpenOption.CREATE,
-                                     StandardOpenOption.WRITE
+                                     StandardOpenOption.WRITE,
+                                     StandardOpenOption.TRUNCATE_EXISTING
                              )))) {
             dataWriter.writeInt(map.size());
             offsetsWriter.writeInt(map.size());
@@ -47,6 +50,38 @@ public class DaoWriter {
                     dataWriter.writeUTF(entry.value());
                 }
                 offsetsWriter.writeLong(dataWriter.size());
+            }
+        }
+    }
+
+    public void writeTmp(Iterator<BaseEntry<String>> iterator, int size) throws IOException {
+        try (DataOutputStream dataWriter = new DataOutputStream(
+                new BufferedOutputStream(
+                        Files.newOutputStream(
+                                pathToDataFile,
+                                StandardOpenOption.CREATE,
+                                StandardOpenOption.WRITE,
+                                StandardOpenOption.TRUNCATE_EXISTING
+                        )));
+             DataOutputStream offsetsWriter = new DataOutputStream(
+                     new BufferedOutputStream(
+                             Files.newOutputStream(
+                                     pathToOffsetsFile,
+                                     StandardOpenOption.CREATE,
+                                     StandardOpenOption.WRITE,
+                                     StandardOpenOption.TRUNCATE_EXISTING
+                             )))) {
+            dataWriter.writeInt(size);
+            offsetsWriter.writeInt(size);
+            offsetsWriter.writeLong(dataWriter.size());
+            while (iterator.hasNext()) {
+                BaseEntry<String> nextEntry = iterator.next();
+                if (nextEntry != null) {
+                    dataWriter.writeUTF(nextEntry.key());
+                    dataWriter.writeBoolean(true);
+                    dataWriter.writeUTF(nextEntry.value());
+                    offsetsWriter.writeLong(dataWriter.size());
+                }
             }
         }
     }
