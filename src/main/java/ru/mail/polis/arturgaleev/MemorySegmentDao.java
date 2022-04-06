8480d0e243e2c481933b7a2a39cab8d08269b339
CORADEAD <50422378+coradead@users.noreply.github.com>
2022-04-07 00:24:46 +0300

    Stage 4 fix

--- src/main/java/ru/mail/polis/arturgaleev/MemorySegmentDao.java
+++ src/main/java/ru/mail/polis/arturgaleev/MemorySegmentDao.java
@@ -45,7 +45,7 @@ public class MemorySegmentDao implements Dao<MemorySegment, Entry<MemorySegment>
                 dataBaseIterator = dataBase.subMap(from, to).values().iterator();
             }
 
-            if (reader.hasReaders()) {
+            if (!reader.hasReaders()) {
                 return dataBaseIterator;
             } else {
                 return new MergeIterator<>(
