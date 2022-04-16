c8d5f1b157f3d4fd44abb3a8ba6beb74175ecbb5
Komissarov Gleb <gl.k@aol.com>
2022-04-17 02:18:10 +0300

    Restart tests

--- src/main/java/ru/mail/polis/test/glebkomissarov/MemorySegmentDaoFactory.java
+++ src/main/java/ru/mail/polis/test/glebkomissarov/MemorySegmentDaoFactory.java
@@ -10,7 +10,7 @@ import ru.mail.polis.test.DaoFactory;
 
 import java.io.IOException;
 
-@DaoFactory(stage = 4, week = 100500)
+@DaoFactory(stage = 4, week = 1005000)
 public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment, BaseEntry<MemorySegment>> {
     @Override
     public Dao<MemorySegment, BaseEntry<MemorySegment>> createDao(Config config) throws IOException {
