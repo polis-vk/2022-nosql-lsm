50b0b0e8ad102a4c782573550dba8729662b6b2c
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2022-04-20 19:57:02 +0300

    Stage 5

--- src/main/java/ru/mail/polis/alinashestakova/InMemoryDao.java
+++ src/main/java/ru/mail/polis/alinashestakova/InMemoryDao.java
@@ -12,6 +12,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -19,10 +23,18 @@ public class InMemoryDao implements Dao<MemorySegment, BaseEntry<MemorySegment>>
 
     private static final MemorySegment VERY_FIRST_KEY = MemorySegment.ofArray(new byte[]{});
 
-    private final ConcurrentNavigableMap<MemorySegment, BaseEntry<MemorySegment>> memory =
+    private ConcurrentNavigableMap<MemorySegment, BaseEntry<MemorySegment>> memory =
             new ConcurrentSkipListMap<>(MemorySegmentComparator.INSTANCE);
+    private ConcurrentNavigableMap<MemorySegment, BaseEntry<MemorySegment>> flushingMemory =
+            new ConcurrentSkipListMap<>(MemorySegmentComparator.INSTANCE);
+    private final AtomicLong memorySize = new AtomicLong();
 
-    private final Storage storage;
+    private final AtomicBoolean isClosed = new AtomicBoolean();
+
+    private final ExecutorService flushExecutorService = Executors.newSingleThreadExecutor();
+    private final ExecutorService compactExecutorService = Executors.newSingleThreadExecutor();
+
+    private Storage storage;
 
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private final Config config;
@@ -30,54 +42,67 @@ public class InMemoryDao implements Dao<MemorySegment, BaseEntry<MemorySegment>>
     public InMemoryDao(Config config) throws IOException {
         this.config = config;
         this.storage = Storage.load(config);
+        isClosed.set(false);
     }
 
     @Override
     public Iterator<BaseEntry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        MemorySegment keyFrom = from;
-        if (keyFrom == null) {
-            keyFrom = VERY_FIRST_KEY;
+        if (isClosed.get()) {
+            throw new IllegalStateException("Dao is closed. You can't get entries range");
         }
 
-        Iterator<BaseEntry<MemorySegment>> memoryIterator = getMemoryIterator(keyFrom, to);
-        Iterator<BaseEntry<MemorySegment>> iterator = storage.iterate(keyFrom, to);
-
-        Iterator<BaseEntry<MemorySegment>> mergeIterator = MergeIterator.of(
-                List.of(
-                        new IndexedPeekIterator<>(0, memoryIterator),
-                        new IndexedPeekIterator<>(1, iterator)
-                ),
-                EntryKeyComparator.INSTANCE
-        );
-
-        IndexedPeekIterator<BaseEntry<MemorySegment>> delegate = new IndexedPeekIterator<>(0, mergeIterator);
-
-        return new Iterator<>() {
-            @Override
-            public boolean hasNext() {
-                while (delegate.hasNext() && delegate.peek().value() == null) {
-                    delegate.next();
-                }
-                return delegate.hasNext();
+        lock.readLock().lock();
+        try {
+            MemorySegment keyFrom = from;
+            if (keyFrom == null) {
+                keyFrom = VERY_FIRST_KEY;
             }
 
-            @Override
-            public BaseEntry<MemorySegment> next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException("...");
+            Iterator<BaseEntry<MemorySegment>> memoryIterator = getMemoryIterator(keyFrom, to, false);
+            Iterator<BaseEntry<MemorySegment>> flushingMemoryIterator = getMemoryIterator(keyFrom, to, true);
+            Iterator<BaseEntry<MemorySegment>> iterator = storage.iterate(keyFrom, to);
+
+            Iterator<BaseEntry<MemorySegment>> mergeIterator = MergeIterator.of(
+                    List.of(
+                            new IndexedPeekIterator<>(0, memoryIterator),
+                            new IndexedPeekIterator<>(1, flushingMemoryIterator),
+                            new IndexedPeekIterator<>(2, iterator)
+                    ),
+                    EntryKeyComparator.INSTANCE
+            );
+
+            IndexedPeekIterator<BaseEntry<MemorySegment>> delegate = new IndexedPeekIterator<>(0, mergeIterator);
+
+            return new Iterator<>() {
+                @Override
+                public boolean hasNext() {
+                    while (delegate.hasNext() && delegate.peek().value() == null) {
+                        delegate.next();
+                    }
+                    return delegate.hasNext();
                 }
-                return delegate.next();
-            }
-        };
+
+                @Override
+                public BaseEntry<MemorySegment> next() {
+                    if (!hasNext()) {
+                        throw new NoSuchElementException("...");
+                    }
+                    return delegate.next();
+                }
+            };
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
-    private Iterator<BaseEntry<MemorySegment>> getMemoryIterator(MemorySegment from, MemorySegment to) {
+    private Iterator<BaseEntry<MemorySegment>> getMemoryIterator(MemorySegment from, MemorySegment to,
+                                                                 boolean isFlushingMemory) {
         lock.readLock().lock();
         try {
             if (to == null) {
-                return memory.tailMap(from).values().iterator();
+                return (isFlushingMemory ? flushingMemory : memory).tailMap(from).values().iterator();
             }
-            return memory.subMap(from, to).values().iterator();
+            return (isFlushingMemory ? flushingMemory : memory).subMap(from, to).values().iterator();
         } finally {
             lock.readLock().unlock();
         }
@@ -85,54 +110,135 @@ public class InMemoryDao implements Dao<MemorySegment, BaseEntry<MemorySegment>>
 
     @Override
     public BaseEntry<MemorySegment> get(MemorySegment key) {
-        Iterator<BaseEntry<MemorySegment>> iterator = get(key, null);
-        if (!iterator.hasNext()) {
-            return null;
+        if (isClosed.get()) {
+            throw new IllegalStateException("Dao is closed. You can't get entry");
         }
-        BaseEntry<MemorySegment> next = iterator.next();
-        if (MemorySegmentComparator.INSTANCE.compare(key, next.key()) == 0) {
-            return next;
-        }
-        return null;
-    }
 
-    @Override
-    public void upsert(BaseEntry<MemorySegment> entry) {
         lock.readLock().lock();
         try {
-            memory.put(entry.key(), entry);
+            Iterator<BaseEntry<MemorySegment>> iterator = get(key, null);
+            if (!iterator.hasNext()) {
+                return null;
+            }
+            BaseEntry<MemorySegment> next = iterator.next();
+            if (MemorySegmentComparator.INSTANCE.compare(key, next.key()) == 0) {
+                return next;
+            }
+            return null;
         } finally {
             lock.readLock().unlock();
         }
     }
 
     @Override
-    public void compact() throws IOException {
-        lock.writeLock().lock();
+    public void upsert(BaseEntry<MemorySegment> entry) {
+        if (isClosed.get()) {
+            throw new IllegalStateException("Dao is closed. You can't upsert entry");
+        }
+
+        lock.readLock().lock();
         try {
-            if (memory.isEmpty() && Storage.getFilesCount(config) <= 1) {
+            long entrySize = Long.BYTES + entry.key().byteSize() + Long.BYTES
+                    + (entry.value() == null ? 0 : entry.key().byteSize());
+
+            if (memorySize.get() + entrySize > config.flushThresholdBytes()) {
+                if (!flushingMemory.isEmpty()) {
+                    throw new IllegalStateException("Memory is full. You can't upsert entry");
+                }
+
+                flushingMemory = memory;
+                memory = createMemoryStorage();
+                memory.put(entry.key(), entry);
+                flushExecutorService.execute(this::autoFlush);
                 return;
             }
 
-            Iterator<BaseEntry<MemorySegment>> allDataIterator = get(null, null);
-            Path tmp = Storage.save(config, storage, allDataIterator);
-            memory.clear();
+            memory.put(entry.key(), entry);
+            memorySize.addAndGet(entrySize);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
 
-            if (tmp != null) {
-                Storage.deleteFiles(config);
-                Storage.moveFile(config, tmp, 0);
-            }
+    private void autoFlush() {
+        lock.writeLock().lock();
+        try {
+            flushOperation(flushingMemory);
+            flushingMemory = createMemoryStorage();
+        } catch (IOException e) {
+            throw new RuntimeException("Error during autoFlush");
         } finally {
             lock.writeLock().unlock();
         }
     }
 
     @Override
-    public void close() throws IOException {
-        if (storage.isClosed()) {
+    public void flush() throws IOException {
+        if (isClosed.get()) {
+            throw new IllegalStateException("Dao is closed. You can't flush");
+        }
+
+        lock.writeLock().lock();
+        try {
+            flushOperation(memory);
+            memory = createMemoryStorage();
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    private void flushOperation(ConcurrentNavigableMap<MemorySegment, BaseEntry<MemorySegment>> map)
+            throws IOException {
+        if (storage.isClosed() || map.isEmpty()) {
             return;
         }
 
+        storage.close();
+        Path tmp = Storage.save(config, storage, map.values().iterator());
+
+        if (tmp != null) {
+            Storage.moveFile(config, tmp, Storage.getFilesCount(config) - 1);
+        }
+        storage = Storage.load(config);
+    }
+
+    @Override
+    public void compact() throws IOException {
+        if (isClosed.get()) {
+            throw new IllegalStateException("Dao is closed. You can't compact");
+        }
+
+        lock.writeLock().lock();
+        try {
+            compactExecutorService.execute(() -> {
+                try {
+                    if (memory.isEmpty() && Storage.getFilesCount(config) <= 1) {
+                        return;
+                    }
+
+                    Iterator<BaseEntry<MemorySegment>> allDataIterator = storage.iterate(null, null);
+                    Path tmp = Storage.save(config, storage, allDataIterator);
+
+                    if (tmp != null) {
+                        Storage.deleteFiles(config);
+                        Storage.moveFile(config, tmp, 0);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException("Error during compaction");
+                }
+            });
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public synchronized void close() throws IOException {
+        if (storage.isClosed() || isClosed.get()) {
+            return;
+        }
+
+        isClosed.set(true);
         storage.close();
         lock.writeLock().lock();
         try {
@@ -141,8 +247,15 @@ public class InMemoryDao implements Dao<MemorySegment, BaseEntry<MemorySegment>>
             if (tmp != null) {
                 Storage.moveFile(config, tmp, Storage.getFilesCount(config) - 1);
             }
+
+            flushExecutorService.shutdown();
+            compactExecutorService.shutdown();
         } finally {
             lock.writeLock().unlock();
         }
+    }
+
+    private static ConcurrentSkipListMap<MemorySegment, BaseEntry<MemorySegment>> createMemoryStorage() {
+        return new ConcurrentSkipListMap<>(ru.mail.polis.artyomdrozdov.MemorySegmentComparator.INSTANCE);
     }
 }
